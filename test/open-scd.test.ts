32b472901e9be38b7ff282cbc43bfb6e1a888aed
Jakob Vogelsang <jakob.vogelsang@omicronenergy.com>
2020-07-27 11:17:26 +0200

    Add test mwc drawer

--- test/open-scd.test.ts
+++ test/open-scd.test.ts
@@ -15,6 +15,21 @@ describe('open-scd', () => {
     expect(linearProgress).to.exist;
   });
 
+  it('toggles a drawer on menu button click', () => {
+    const drawer = element.shadowRoot!.querySelector('mwc-drawer');
+    expect(drawer).to.exist;
+    expect(drawer).to.not.be.visible;
+    const menuButton = <HTMLElement>(
+      element.shadowRoot!.querySelector(
+        'mwc-icon-button[slot="navigationIcon"]'
+      )
+    );
+    menuButton.click();
+    expect(drawer).to.be.visible;
+    menuButton.click();
+    expect(drawer).to.not.be.visible;
+  });
+
   it('passes the a11y audit', async () => {
     await expect(element).shadowDom.to.be.accessible();
   });
