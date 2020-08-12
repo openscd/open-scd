327b63a2bd6b3b6777d05347803da2811383ab73
Christian Hildebrandt <christian.hildebrandt@omicronenergy.com>
2020-08-12 22:09:26 +0200

    Remove headless browser from test conf

--- karma.conf.cjs
+++ karma.conf.cjs
@@ -2,37 +2,48 @@
 const { createDefaultConfig } = require('@open-wc/testing-karma');
 const merge = require('deepmerge');
 process.env.CHROME_BIN = require('puppeteer').executablePath();
+const overwriteMerge = (destinationArray, sourceArray, options) => sourceArray;
 
 module.exports = config => {
   config.set(
-    merge(createDefaultConfig(config), {
-      browserDisconnectTimeout: 20000,
-      files: [
-        'xmllint.js',
-        // runs all files ending with .test in the test folder,
-        // can be overwritten by passing a --grep flag. examples:
-        //
-        // npm run test -- --grep test/foo/bar.test.js
-        // npm run test -- --grep test/bar/*
-        {
-          pattern: config.grep ? config.grep : 'out-tsc/**/test/**/*.test.js',
-          type: 'module',
-        },
-      ],
+    merge(
+      createDefaultConfig(config),
+      {
+        browserDisconnectTimeout: 20000,
+        files: [
+          'xmllint.js',
+          // runs all files ending with .test in the test folder,
+          // can be overwritten by passing a --grep flag. examples:
+          //
+          // npm run test -- --grep test/foo/bar.test.js
+          // npm run test -- --grep test/bar/*
+          {
+            pattern: config.grep ? config.grep : 'out-tsc/**/test/**/*.test.js',
+            type: 'module',
+          },
+        ],
 
-      browsers: ['ChromeHeadlessNoSandbox'],
-      customLaunchers: {
-        ChromeHeadlessNoSandbox: {
-          base: 'ChromeHeadless',
-          flags: ['--no-sandbox'],
+        browsers: [], // ['ChromeHeadlessNoSandbox'], // commented for WSL
+        customLaunchers: {
+          ChromeHeadlessNoSandbox: {
+            base: 'ChromeHeadless',
+            flags: ['--no-sandbox'],
+          },
         },
-      },
 
-      esm: {
-        nodeResolve: true,
+        esm: {
+          nodeResolve: true,
+        },
+        // you can overwrite/extend the config further
       },
-      // you can overwrite/extend the config further
-    })
+      {
+        customMerge: key => {
+          if (key === 'browsers') {
+            return overwriteMerge;
+          }
+        },
+      }
+    )
   );
   return config;
 };
