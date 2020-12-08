ca308c5953b30f3e99dcfbb8e8885987f8f8c813
Jakob Vogelsang! <jakob.vogelsang@omicronenergy.com>
2020-12-08 11:09:31 +0100

    refactor(foundation): add regular expression for typical xml types

--- src/foundation.ts
+++ src/foundation.ts
@@ -272,3 +272,23 @@ declare global {
     ['log']: LogEvent;
   }
 }
+
+const nameStartChar =
+  ':|[A-Z]|_|[a-z]|[\u00C0-\u00D6]|[\u00D8-\u00F6]|[\u00F8-\u02FF]|[\u0370-\u037D]|[\u037F-\u1FFF]|[\u200C-\u200D]|[\u2070-\u218F]|[\u2C00-\u2FEF]|[\u3001-\uD7FF]|[\uF900-\uFDCF]|[\uFDF0-\uFFFD]|[\u{10000}\\-\u{EFFFF}]';
+const nameChar =
+  nameStartChar + '|-|.|[0-9]|\u00B7|[\u0300-\u036F]|[\u203F-\u2040]';
+const name = nameStartChar + ' (' + nameChar + ')*';
+const nmToken = '(' + nameChar + ')+';
+
+export const restrictions = {
+  string:
+    '([\u0009-\u000A]|[\u000D]|[\u0020-\u007E]|[\u0085]|[\u00A0-\uD7FF]|[\uE000-\uFFFD]|[\u{10000}\\-\u{10FFFF}])*',
+  normalizedString:
+    '([\u0020-\u007E]|[\u0085]|[\u00A0-\uD7FF]|[\uE000-\uFFFD]|[\u{10000}\\-\u{10FFFF}])*',
+  name: nameStartChar + ' (' + nameChar + ')*',
+  names: name + ' (\u0020 ' + name + ')*',
+  nmToke: '(' + nameChar + ')+',
+  nmTokens: nmToken + ' (\u0020 ' + nmToken + ')*',
+  decimal: '((-|\\+)?([0-9]+(.[0-9]*)?|.[0-9]+))',
+  min0decimal: '([0-9]+(.[0-9]*)?|.[0-9]+)',
+};
