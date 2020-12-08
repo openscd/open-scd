39f0901cf56aef71d318daf4850cea2408976522
Jakob Vogelsang! <jakob.vogelsang@omicronenergy.com>
2020-12-08 11:24:24 +0100

    refactor(foundation): add regular expressions for property based testing

--- test/foundation.ts
+++ test/foundation.ts
@@ -1,4 +1,7 @@
+/* eslint-disable no-control-regex */
+
 import fc, { Arbitrary } from 'fast-check';
+import { restrictions } from '../src/foundation.js';
 
 export function regexString(
   re: RegExp,
@@ -10,10 +13,20 @@ export function regexString(
     .filter(char => re.test(char));
 }
 
-export const restrictions = {
+export const regExp = {
   tIEDName: /^[A-Za-z][0-9A-Za-z_]*$/,
   tLDInst: /^[A-Za-z][0-9A-Za-z_]*$/,
   tPrefix: /^[A-Za-z][0-9A-Za-z_]*$/,
   tLNClass: /^[A-Z]{1,4}$/,
   tLNInst: /^[0-9]{0,12}$/,
+  decimal: new RegExp(`^${restrictions.decimal}$`),
+  min0decimal: new RegExp(`^${restrictions.min0decimal}$`),
+  tName: new RegExp(`^${restrictions.normalizedString}$`),
+  desc: new RegExp(`^${restrictions.normalizedString}$`),
+};
+
+export const negativeRegExp = {
+  min0decimal: /^(?!.*[-+0-9.]).*$/,
+  decimal: /^(?!.*[0-9.]).*$/,
+  integer: /^(?!.*[0-9]).*$/,
 };
