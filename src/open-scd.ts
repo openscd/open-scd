a25a41a812fd2eabc2e04311b3b66aec5f47f604
Christian Dinkel <christian.dinkel@omicronenergy.com>
2020-12-09 20:05:02 +0100

    feat: add save and saveAs keyboard shortcuts

--- src/open-scd.ts
+++ src/open-scd.ts
@@ -163,6 +163,8 @@ export class OpenSCD extends Setting(
     if (ctrlAnd('l')) this.logUI.open ? this.logUI.close() : this.logUI.show();
     if (ctrlAnd('m')) this.menuUI.open = !this.menuUI.open;
     if (ctrlAnd('o')) this.fileUI.click();
+    if (ctrlAnd('s')) this.save();
+    if (ctrlAnd('S')) this.saveUI.show();
 
     if (handled) e.preventDefault();
   }
