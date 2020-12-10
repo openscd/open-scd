9cffe192f6608ffd19658c2c06a3b4eed10b6a4e
Christian Dinkel <christian.dinkel@omicronenergy.com>
2020-12-10 13:04:49 +0100

    feat: auto-generate service worker with workbox

--- sw.js
+++ sw.js
@@ -1,37 +1,11 @@
-const cacheName = 'open-scd-v1';
-
 self.addEventListener('install', e => {
-  console.log('[Service Worker] installed');
+  console.log('Dev (dummy, non-caching) service worker installed');
 });
 
 self.addEventListener('fetch', e => {
-  /* // uncomment to activate caching for offline installation
-  e.respondWith(
-    caches.match(e.request).then(r => {
-      return (
-        r ||
-        fetch(e.request).then(response => {
-          return caches.open(cacheName).then(cache => {
-            cache.put(e.request, response.clone());
-            return response;
-          });
-        })
-      );
-    })
-  );
-  */
+  e.respondWith(fetch(e.request));
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(
-        keyList.map(key => {
-          if (key !== cacheName) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
+  console.log('Dev (dummy, non-caching) service worker activated');
 });
---
+++ workbox-config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  globDirectory: 'build/',
+  globPatterns: [
+    'public/**/*.{md,js,png,xml,pdf,css,html,info,json,ico,svg,wasm}',
+    'src/**/*.{md,js,png,xml,pdf,css,html,info,json,ico,svg,wasm}',
+    'web_modules/**/*.{md,js,png,xml,pdf,css,html,info,json,ico,svg,wasm}',
+    '*.{md,js,png,xml,pdf,css,html,info,json,ico,svg,wasm}',
+  ],
+  swDest: 'build/sw.js',
+  runtimeCaching: [
+    {
+      urlPattern: /.*/,
+      handler: 'NetworkFirst',
+    },
+  ],
+  skipWaiting: true,
+  inlineWorkboxRuntime: true,
+};
