567bf7404f9cdc2076d84d0b8ece1dde4a0a208c
Jakob Vogelsang <jakob.vogelsang@omicronenergy.com>
2021-01-27 00:26:17 +0100

    fix(foundation): add namespace info to createElement function

--- src/foundation.ts
+++ src/foundation.ts
@@ -264,7 +264,7 @@ export function createElement(
   tag: string,
   attrs: Record<string, string | null>
 ): Element {
-  const element = doc.createElement(tag);
+  const element = doc.createElementNS(doc.documentElement.namespaceURI, tag);
   Object.entries(attrs)
     .filter(([_, value]) => value !== null)
     .forEach(([name, value]) => element.setAttribute(name, value!));
