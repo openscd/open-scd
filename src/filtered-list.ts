378be07a650408434d992f7c82495372d737669a
Jakob Vogelsang <jakob-vogelsang@posteo.de>
2021-03-08 13:20:21 +0100

    feat(filtered-list): add custom element based on mwc-list

---
+++ src/filtered-list.ts
@@ -0,0 +1,53 @@
+import { List } from '@material/mwc-list';
+import { TextField } from '@material/mwc-textfield';
+import {
+  css,
+  customElement,
+  html,
+  property,
+  query,
+  TemplateResult,
+} from 'lit-element';
+
+@customElement('filtered-list')
+export class Filterlist extends List {
+  @property()
+  searchFieldLabel!: string;
+
+  @query('mwc-textfield') textField!: TextField;
+
+  onFilterInput(): void {
+    this.items.forEach(item => {
+      const value: string =
+        item.innerText +
+        Array.from(item.children)
+          .map(child => child.innerHTML)
+          .join('');
+
+      if (value)
+        value.toUpperCase().includes(this.textField.value.toUpperCase())
+          ? item.removeAttribute('style')
+          : item.setAttribute('style', 'display:none;');
+    });
+  }
+
+  render(): TemplateResult {
+    return html`<mwc-textfield
+        label="${this.searchFieldLabel ?? ''}"
+        iconTrailing="search"
+        outlined
+        @input=${() => this.onFilterInput()}
+      ></mwc-textfield
+      >${super.render()}`;
+  }
+
+  static styles = css`
+    mwc-textfield {
+      margin: 10px;
+      --mdc-shape-small: 28px;
+    }
+    .mdc-list {
+      padding-left: 16px;
+    }
+  `;
+}
