87b25f9cc0e1cb6d6a57090b95d3f6746e5f22ab
Jakob Vogelsang <jakob-vogelsang@posteo.de>
2021-03-28 15:02:42 +0200

    feat(icons): add goose, report and smv service icons

--- src/icons.ts
+++ src/icons.ts
@@ -1,5 +1,17 @@
 import { html, svg, TemplateResult } from 'lit-element';
 
+export const gooseIcon = svg`<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+<path fill="currentColor" d="M11,7H15V9H11V15H13V11H15V15A2,2 0 0,1 13,17H11A2,2 0 0,1 9,15V9A2,2 0 0,1 11,7M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2M12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20A8,8 0 0,0 20,12A8,8 0 0,0 12,4Z" />
+</svg>`;
+
+export const reportIcon = svg`<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+    <path fill="currentColor" d="M9,7H13A2,2 0 0,1 15,9V11C15,11.84 14.5,12.55 13.76,12.85L15,17H13L11.8,13H11V17H9V7M11,9V11H13V9H11M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2M12,4A8,8 0 0,0 4,12C4,16.41 7.58,20 12,20A8,8 0 0,0 20,12A8,8 0 0,0 12,4Z" />
+</svg>`;
+
+export const smvIcon = svg`<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+<path fill="currentColor" d="M11,7H15V9H11V11H13A2,2 0 0,1 15,13V15A2,2 0 0,1 13,17H9V15H13V13H11A2,2 0 0,1 9,11V9A2,2 0 0,1 11,7M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2M12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20A8,8 0 0,0 20,12A8,8 0 0,0 12,4Z" />
+</svg>`;
+
 const pathsSVG = {
   action: svg`<path d="M0 0h24v24H0z" fill="none"></path><path d="M13 3c-4.97 0-9
   4.03-9 9H1l3.89 3.89.07.14L9 12H6c0-3.87 3.13-7 7-7s7 3.13 7 7-3.13 7-7
