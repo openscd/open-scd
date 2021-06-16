27b4469478d6dc96fc02f5bba684d68d583e54ba
Jakob Vogelsang <jakob.vogelsang@omicronenergy.com>
2021-06-16 12:03:37 +0200

    test(validating): fetch test info from testfiles

--- test/unit/Validating.test.ts
+++ test/unit/Validating.test.ts
@@ -1,13 +1,19 @@
 import { html, fixture, expect } from '@open-wc/testing';
 
 import { ValidatingElement } from '../../src/Validating.js';
-
-import { getDocument } from '../data.js';
 import './mock-validator.js';
 
 describe('ValidatingElement', () => {
   let element: ValidatingElement;
+  let validSCL: XMLDocument;
+  let invalidSCL: XMLDocument;
   beforeEach(async () => {
+    validSCL = await fetch('/base/test/testfiles/valid.scd')
+      .then(response => response.text())
+      .then(str => new DOMParser().parseFromString(str, 'application/xml'));
+    invalidSCL = await fetch('/base/test/testfiles/invalidSCL.scd')
+      .then(response => response.text())
+      .then(str => new DOMParser().parseFromString(str, 'application/xml'));
     element = <ValidatingElement>(
       await fixture(html`<mock-validator></mock-validator>`)
     );
@@ -19,12 +25,12 @@ describe('ValidatingElement', () => {
   });
 
   it('validates a valid SCL document', async () => {
-    element.validate(getDocument(), { fileName: 'valid.scd' });
+    element.validate(validSCL, { fileName: 'valid.scd' });
     expect(await element.validated).to.have.property('valid', true);
   });
 
   it('does not validate an invalid SCL document', async () => {
-    element.validate(getDocument(false), { fileName: 'invalid.scd' });
+    element.validate(invalidSCL, { fileName: 'invalid.scd' });
     expect(await element.validated).to.have.property('valid', false);
   });
 });
