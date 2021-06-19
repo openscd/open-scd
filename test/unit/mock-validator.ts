edbdb7f59d8dcf61bac3dfc36e556472a463e81b
Jakob Vogelsang <jakob.vogelsang@omicronenergy.com>
2021-06-19 16:58:44 +0200

    test(validators/SchemaValidator): add integration test

--- test/unit/mock-validator.ts
+++
@@ -1,5 +0,0 @@
-/* import { Validating } from '../../src/Validating.js';
-import { LitElement, customElement } from 'lit-element';
-
-@customElement('mock-validator')
-export class MockValidator extends Validating(LitElement) {} */
