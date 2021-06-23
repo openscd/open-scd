86b9c1c36ca0923bd8dfaf5a5a4cf3f9f1945bb2
Dennis Labordus <dennis.labordus@alliander.com>
2021-06-23 08:19:45 +0200

    Required is ignored by MWC.
    Signed-off-by: Dennis Labordus <dennis.labordus@alliander.com>

--- src/compas/CompasChangeSet.ts
+++ src/compas/CompasChangeSet.ts
@@ -34,7 +34,6 @@ export class CompasChangeSetRadiogroup extends LitElement {
     return this.getSelectedListItem() !== null;
   }
 
-
   render(): TemplateResult {
     return html`
       <mwc-list activatable>
