da99118212797debe6ab3e7b29a1debae169fff8
Jakob Vogelsang <jakob.vogelsang@omicronenergy.com>
2021-06-24 15:10:08 +0200

    feat(validators/validatetemplates): validate for mendatory DAs within DOType

--- src/validators/ValidateTemplates.ts
+++ src/validators/ValidateTemplates.ts
@@ -5,6 +5,39 @@ const iec6185074 = fetch('public/xml/IEC_61850-7-4_2007B3.nsd')
   .then(response => response.text())
   .then(str => new DOMParser().parseFromString(str, 'application/xml'));
 
+const iec6185073 = fetch('public/xml/IEC_61850-7-3_2007B3.nsd')
+  .then(response => response.text())
+  .then(str => new DOMParser().parseFromString(str, 'application/xml'));
+
+async function getMendatoryDataAttribute(base: string): Promise<Element[]> {
+  const nsd = await iec6185073;
+  const cdc = nsd.querySelector(`CDC[name="${base}"]`);
+
+  if (!cdc) return [];
+
+  return Array.from(cdc.querySelectorAll('DataAttribute[presCond="M"]'));
+}
+
+async function validateMandatoryDAs(dotype: Element): Promise<LogDetail[]> {
+  const errors: LogDetail[] = [];
+  const cdc = dotype.getAttribute('cdc');
+  if (!cdc) return [];
+
+  const mandatorydas = await (
+    await getMendatoryDataAttribute(cdc)
+  ).map(DA => DA.getAttribute('name')!);
+
+  mandatorydas.forEach(mandatoryda => {
+    if (!dotype.querySelector(`DA[name="${mandatoryda}"]`))
+      errors.push({
+        title: `The element DA ${mandatoryda} is mendatory in Common Data Class ${cdc}`,
+        kind: 'error',
+      });
+  });
+
+  return errors;
+}
+
 function getAdjacentClass(nsd: XMLDocument, base: string): Element[] {
   if (base === '') return [];
   const adjacents = getAdjacentClass(
@@ -78,7 +111,7 @@ async function getMendatoryDataObject(base: string): Promise<Element[]> {
   );
 }
 
-async function validateMandatoryDo(lnodetype: Element): Promise<LogDetail[]> {
+async function validateMandatoryDOs(lnodetype: Element): Promise<LogDetail[]> {
   const errors: LogDetail[] = [];
   const lnClass = lnodetype.getAttribute('lnClass');
   if (!lnClass) return [];
@@ -107,13 +140,21 @@ export default class ValidateTemplates extends LitElement {
 
   async validate(): Promise<void> {
     Array.from(this.doc.querySelectorAll('LNodeType')).forEach(lnodetype => {
-      validateMandatoryDo(lnodetype).then(errors => {
+      validateMandatoryDOs(lnodetype).then(errors => {
         errors.forEach(error =>
           document.querySelector('open-scd')?.dispatchEvent(newLogEvent(error))
         );
       });
 
       validateDoCDCSetting(lnodetype).then(errors => {
+        errors.forEach(error =>
+          document.querySelector('open-scd')?.dispatchEvent(newLogEvent(error))
+        );
+      });
+    });
+
+    Array.from(this.doc.querySelectorAll('DOType')).forEach(dotype => {
+      validateMandatoryDAs(dotype).then(errors => {
         errors.forEach(error =>
           document.querySelector('open-scd')?.dispatchEvent(newLogEvent(error))
         );
