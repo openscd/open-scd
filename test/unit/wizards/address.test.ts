79e3db8b6440c72524fb326117100519b240f473
Jakob Vogelsang <jakob.vogelsang@omicronenergy.com>
2021-08-02 13:47:08 +0200

    test(wizards/address): improve unit tests

--- test/unit/wizards/address.test.ts
+++ test/unit/wizards/address.test.ts
@@ -48,91 +48,161 @@ describe('address', () => {
     let inputs: WizardInput[];
     let wizard: Wizard;
 
-    beforeEach(async () => {
-      gse = doc.querySelector(
-        'GSE[ldInst="CircuitBreaker_CB1"][cbName="GCB"]'
-      )!;
+    describe('with exiting address element', () => {
+      beforeEach(async () => {
+        gse = doc.querySelector(
+          'GSE[ldInst="CircuitBreaker_CB1"][cbName="GCB"]'
+        )!;
 
-      wizard = [
-        {
-          title: 'asdas',
-          content: renderGseSmvAddress(gse),
-        },
-      ];
-      element.workflow.push(wizard);
-      await element.requestUpdate();
-      inputs = Array.from(element.wizardUI.inputs);
-      await element.requestUpdate();
-    });
+        wizard = [
+          {
+            title: 'asdas',
+            content: renderGseSmvAddress(gse),
+          },
+        ];
+        element.workflow.push(wizard);
+        await element.requestUpdate();
+        inputs = Array.from(element.wizardUI.inputs);
+        await element.requestUpdate();
+      });
 
-    it('does not update a Address element when no attribute has changed', () => {
-      const actions = updateAddress(gse, inputs, false);
-      expect(actions).to.be.empty;
-    });
-    it('update a Address element when at least one attribute changes', async () => {
-      for (const rawInput of inputs) {
-        const input =
-          rawInput instanceof WizardTextField
-            ? <WizardTextField>rawInput
-            : <WizardSelect>rawInput;
-
-        const type = input.label;
-        const newValue = 'newValue';
-        const oldValue = input.value;
-
-        input.value = newValue;
-        await input.requestUpdate();
+      it('does not update a Address element when no attribute has changed', () => {
         const actions = updateAddress(gse, inputs, false);
-        expect(actions[0]).to.satisfy(isDelete);
-        expect(actions[1]).to.satisfy(isCreate);
-        const oldElement = (<Delete>actions[0]).old.element;
-        const newElement = (<Create>actions[1]).new.element;
-        console.log(type);
-        console.log(oldValue);
-        console.log(newValue);
-        expect(
-          oldElement.querySelector(`P[type="${type}"]`)?.textContent?.trim()
-        ).to.equal(oldValue);
-        expect(
-          newElement.querySelector(`P[type="${type}"]`)?.textContent?.trim()
-        ).to.equal(newValue);
-        expect(
-          newElement.querySelector(`P[type="${type}"]`)
-        ).to.not.have.attribute('xsi:type', `tP_${type}`);
-      }
+        expect(actions).to.be.empty;
+      });
+      it('update a Address element when at least one attribute changes', async () => {
+        for (const rawInput of inputs) {
+          const input =
+            rawInput instanceof WizardTextField
+              ? <WizardTextField>rawInput
+              : <WizardSelect>rawInput;
+
+          const type = input.label;
+          const newValue = 'newValue';
+          const oldValue = input.value;
+
+          input.value = newValue;
+          await input.requestUpdate();
+          const actions = updateAddress(gse, inputs, false);
+          expect(actions.length).to.equal(2);
+          expect(actions[0]).to.satisfy(isDelete);
+          expect(actions[1]).to.satisfy(isCreate);
+          const oldElement = (<Delete>actions[0]).old.element;
+          const newElement = (<Create>actions[1]).new.element;
+          console.log(type);
+          console.log(oldValue);
+          console.log(newValue);
+          expect(
+            oldElement.querySelector(`P[type="${type}"]`)?.textContent?.trim()
+          ).to.equal(oldValue);
+          expect(
+            newElement.querySelector(`P[type="${type}"]`)?.textContent?.trim()
+          ).to.equal(newValue);
+          expect(
+            newElement.querySelector(`P[type="${type}"]`)
+          ).to.not.have.attribute('xsi:type', `tP_${type}`);
+        }
+      });
+      it('update a Address element when status of instType has changed', async () => {
+        for (const rawInput of inputs) {
+          const input =
+            rawInput instanceof WizardTextField
+              ? <WizardTextField>rawInput
+              : <WizardSelect>rawInput;
+
+          const type = input.label;
+          const newValue = input.value;
+          const oldValue = input.value;
+
+          input.value = newValue;
+          await input.requestUpdate();
+          const actions = updateAddress(gse, inputs, true);
+          expect(actions.length).to.equal(2);
+          expect(actions[0]).to.satisfy(isDelete);
+          expect(actions[1]).to.satisfy(isCreate);
+          const oldElement = (<Delete>actions[0]).old.element;
+          const newElement = (<Create>actions[1]).new.element;
+          console.log(type);
+          console.log(oldValue);
+          console.log(newValue);
+          expect(
+            oldElement.querySelector(`P[type="${type}"]`)?.textContent?.trim()
+          ).to.equal(oldValue);
+          expect(
+            newElement.querySelector(`P[type="${type}"]`)?.textContent?.trim()
+          ).to.equal(newValue);
+          expect(
+            newElement.querySelector(`P[type="${type}"]`)
+          ).to.have.attribute('xsi:type', `tP_${type}`);
+        }
+      });
     });
-    it('update a Address element when status of instType has changed', async () => {
-      for (const rawInput of inputs) {
-        const input =
-          rawInput instanceof WizardTextField
-            ? <WizardTextField>rawInput
-            : <WizardSelect>rawInput;
+    describe('with missing address element', () => {
+      beforeEach(async () => {
+        gse = doc.querySelector(
+          'GSE[ldInst="CircuitBreaker_CB1"][cbName="GCB2"]'
+        )!;
 
-        const type = input.label;
-        const newValue = input.value;
-        const oldValue = input.value;
+        wizard = [
+          {
+            title: 'asdas',
+            content: renderGseSmvAddress(gse),
+          },
+        ];
+        element.workflow.push(wizard);
+        await element.requestUpdate();
+        inputs = Array.from(element.wizardUI.inputs);
+        await element.requestUpdate();
+      });
 
-        input.value = newValue;
-        await input.requestUpdate();
-        const actions = updateAddress(gse, inputs, true);
-        expect(actions[0]).to.satisfy(isDelete);
-        expect(actions[1]).to.satisfy(isCreate);
-        const oldElement = (<Delete>actions[0]).old.element;
-        const newElement = (<Create>actions[1]).new.element;
-        console.log(type);
-        console.log(oldValue);
-        console.log(newValue);
-        expect(
-          oldElement.querySelector(`P[type="${type}"]`)?.textContent?.trim()
-        ).to.equal(oldValue);
-        expect(
-          newElement.querySelector(`P[type="${type}"]`)?.textContent?.trim()
-        ).to.equal(newValue);
-        expect(newElement.querySelector(`P[type="${type}"]`)).to.have.attribute(
-          'xsi:type',
-          `tP_${type}`
-        );
-      }
+      it('creates a Address element when at least one attribute changes', async () => {
+        for (const rawInput of inputs) {
+          const input =
+            rawInput instanceof WizardTextField
+              ? <WizardTextField>rawInput
+              : <WizardSelect>rawInput;
+
+          const type = input.label;
+          const newValue = 'newValue';
+
+          input.value = newValue;
+          await input.requestUpdate();
+          const actions = updateAddress(gse, inputs, false);
+          expect(actions.length).to.equal(1);
+          expect(actions[0]).to.satisfy(isCreate);
+          const newElement = (<Create>actions[0]).new.element;
+          expect(
+            newElement.querySelector(`P[type="${type}"]`)?.textContent?.trim()
+          ).to.equal(newValue);
+          expect(
+            newElement.querySelector(`P[type="${type}"]`)
+          ).to.not.have.attribute('xsi:type', `tP_${type}`);
+        }
+      });
+      it('update a Address element when status of instType has changed', async () => {
+        for (const rawInput of inputs) {
+          const input =
+            rawInput instanceof WizardTextField
+              ? <WizardTextField>rawInput
+              : <WizardSelect>rawInput;
+
+          const type = input.label;
+          const newValue = input.value;
+
+          input.value = newValue;
+          await input.requestUpdate();
+          const actions = updateAddress(gse, inputs, true);
+          expect(actions.length).to.equal(1);
+          expect(actions[0]).to.satisfy(isCreate);
+          const newElement = (<Create>actions[0]).new.element;
+          expect(
+            newElement.querySelector(`P[type="${type}"]`)?.textContent?.trim()
+          ).to.equal(newValue);
+          expect(
+            newElement.querySelector(`P[type="${type}"]`)
+          ).to.have.attribute('xsi:type', `tP_${type}`);
+        }
+      });
     });
   });
 });
