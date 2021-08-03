7917a52138a68f9a6232c40c9150501ba68da295
Jakob Vogelsang <jakob.vogelsang@omicronenergy.com>
2021-08-03 10:43:56 +0200

    test(wizards/dataset): fix integration test

--- test/integration/wizards/dataset-wizarding-editing-integration.test.ts
+++ test/integration/wizards/dataset-wizarding-editing-integration.test.ts
@@ -2,37 +2,48 @@ import { expect, fixture, html } from '@open-wc/testing';
 
 import { WizardTextField } from '../../../src/wizard-textfield.js';
 import { editDataSetWizard } from '../../../src/wizards/dataset.js';
-
-import { MockWizard } from '../../mock-wizard.js';
+import { MockWizardEditor } from '../../mock-wizard-editor.js';
 
 describe('dataset wizards', () => {
   let doc: XMLDocument;
-  let element: MockWizard;
-  let nameField: WizardTextField;
+  let element: MockWizardEditor;
 
   beforeEach(async () => {
-    element = await fixture(html`<mock-wizard></mock-wizard>`);
+    element = await fixture(html`<mock-wizard-editor></mock-wizard-editor>`);
     doc = await fetch('/base/test/testfiles/wizards/gsecontrol.scd')
       .then(response => response.text())
       .then(str => new DOMParser().parseFromString(str, 'application/xml'));
   });
 
   describe('editDataSetWizard', () => {
+    let primaryAction: HTMLElement;
+    let nameField: WizardTextField;
+
     beforeEach(async () => {
       const wizard = editDataSetWizard(
         doc.querySelector('IED[name="IED2"] DataSet[name="GooseDataSet1"]')!
       );
       element.workflow.push(wizard);
       await element.requestUpdate();
+      primaryAction = <HTMLElement>(
+        element.wizardUI.dialog?.querySelector(
+          'mwc-button[slot="primaryAction"]'
+        )
+      );
       nameField = element.wizardUI.dialog!.querySelector(
         'wizard-textfield[label="name"]'
       )!;
       await nameField.updateComplete;
     });
+
     it('allows to change data set attributes', () => {
-      nameField.value = 'myNewDataSetName';
       expect(
         doc.querySelector('IED[name="IED2"] DataSet[name="myNewDataSet"]')
+      );
+      nameField.value = 'myNewDataSetName';
+      primaryAction.click();
+      expect(
+        doc.querySelector('IED[name="IED2"] DataSet[name="myNewDataSetName"]')
       );
     });
   });
