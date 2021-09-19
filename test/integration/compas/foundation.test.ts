a72b8a454bdc9e493db210b6cbd7e3c90c204f97
Rob Tjalma <rob@tjalma.com>
2021-09-19 23:29:13 +0200

    Added test
    Signed-off-by: Rob Tjalma <rob@tjalma.com>

--- test/integration/compas/foundation.test.ts
+++ test/integration/compas/foundation.test.ts
@@ -4,27 +4,34 @@ import {updateDocumentInOpenSCD} from "../../../src/compas/foundation.js";
 
 import {OpenSCD} from "../../../src/open-scd.js";
 import '../../../src/open-scd.js';
+import { newUserInfoEvent } from '../../../src/foundation.js';
 
 describe('compas-foundation', () => {
-  let doc: XMLDocument;
   let element: OpenSCD;
 
   beforeEach(async () => {
     element = await fixture(
         html`<open-scd></open-scd>`
     );
-
-    doc = await fetch('/base/test/testfiles/compas/test-scd.cid')
-      .then(response => response.text())
-      .then(str => new DOMParser().parseFromString(str, 'application/xml'));
   });
 
   it('when loaded the document should be on open-scd component', async () => {
+    let doc = await fetch('/base/test/testfiles/compas/test-scd.cid')
+      .then(response => response.text())
+      .then(str => new DOMParser().parseFromString(str, 'application/xml'));
+
     updateDocumentInOpenSCD(doc);
     await element.updateComplete;
 
     expect(element.doc).to.be.not.undefined;
     expect(element.docId).to.be.equal('380b5e70-4753-4b59-b5b4-d51ceb26a30c');
     expect(element.docName).to.be.equal('ied_utrecht_station235-3.0.0.cid');
+  });
+
+  it('when UserInfoEvent event is dispatched, the username is shown in OpenSCD', async () => {
+    element.dispatchEvent(newUserInfoEvent("Henk"));
+    await element.updateComplete;
+
+    expect(element.shadowRoot!.querySelector('span[id="userField"]')!.textContent).to.be.equal('Logged in as Henk')
   });
 });
