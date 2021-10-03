7a7e9461628e9b626017f6ba6bdee4ad0647e633
Jakob Vogelsang <jakob.vogelsang@omicronenergy.com>
2021-10-03 23:32:03 +0200

    feat(zeroline/foundation): add create and create option function

--- src/zeroline/foundation.ts
+++ src/zeroline/foundation.ts
@@ -1,18 +1,33 @@
 import { css } from 'lit-element';
 
 import {
-  EditorAction,
-  getValue,
   newActionEvent,
-  WizardActor,
-  WizardInput,
   isPublic,
-  cloneElement,
+  SCLTag,
+  Wizard,
+  tags,
 } from '../foundation.js';
+import { emptyWizard, wizards } from '../wizards/wizard-library.js';
 
 import { BayEditor } from './bay-editor.js';
 import { SubstationEditor } from './substation-editor.js';
 import { VoltageLevelEditor } from './voltage-level-editor.js';
+
+export function getCreateWizard(
+  element: Element
+): (tagName: string) => Wizard | undefined {
+  return (tagName: string) => {
+    if (!tagName || !(tagName as SCLTag)) return;
+
+    return wizards[<SCLTag>tagName].create(element);
+  };
+}
+
+export function getExistingCreateOptions(element: Element): string[] {
+  const allChildren = tags[<SCLTag>element.tagName].children ?? [];
+
+  return allChildren.filter(child => wizards[child].create !== emptyWizard);
+}
 
 function containsReference(element: Element, iedName: string): boolean {
   return Array.from(element.getElementsByTagName('LNode'))
@@ -239,62 +254,6 @@ export const selectors = <Record<SubstationTag, string>>(
 
 /** Common `CSS` styles used by substation subeditors */
 export const styles = css`
-  :host(.moving) section {
-    opacity: 0.3;
-  }
-
-  section {
-    background-color: var(--mdc-theme-surface);
-    transition: all 200ms linear;
-    outline-color: var(--mdc-theme-primary);
-    outline-style: solid;
-    outline-width: 0px;
-    margin: 8px 12px 16px;
-    opacity: 1;
-  }
-
-  section:focus {
-    box-shadow: 0 8px 10px 1px rgba(0, 0, 0, 0.14),
-      0 3px 14px 2px rgba(0, 0, 0, 0.12), 0 5px 5px -3px rgba(0, 0, 0, 0.2);
-  }
-
-  section:focus-within {
-    outline-width: 2px;
-    transition: all 250ms linear;
-  }
-
-  h1,
-  h2,
-  h3 {
-    color: var(--mdc-theme-on-surface);
-    font-family: 'Roboto', sans-serif;
-    font-weight: 300;
-    overflow: hidden;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-    margin: 0px;
-    line-height: 48px;
-    padding-left: 0.3em;
-    transition: background-color 150ms linear;
-  }
-
-  section:focus-within > h1,
-  section:focus-within > h2,
-  section:focus-within > h3 {
-    color: var(--mdc-theme-surface);
-    background-color: var(--mdc-theme-primary);
-    transition: background-color 200ms linear;
-  }
-
-  h1 > nav,
-  h2 > nav,
-  h3 > nav,
-  h1 > abbr > mwc-icon-button,
-  h2 > abbr > mwc-icon-button,
-  h3 > abbr > mwc-icon-button {
-    float: right;
-  }
-
   abbr {
     text-decoration: none;
     border-bottom: none;
