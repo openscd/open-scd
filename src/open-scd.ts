745ff8c39c17ec93d0e2a211108f7b02fb7e5b9d
Christian Dinkel <chhildeb@gmail.com>
2021-10-06 17:57:18 +0200

    refactor(menu/Help): replace finder-pane with finder-list

--- src/open-scd.ts
+++ src/open-scd.ts
@@ -32,10 +32,10 @@ import '@material/mwc-textfield';
 import '@material/mwc-top-app-bar-fixed';
 
 import './filtered-list.js';
+import './finder-list.js';
 import './wizard-dialog.js';
 import './wizard-textfield.js';
 import './wizard-select.js';
-import './finder-pane.js';
 
 import { newOpenDocEvent, newPendingStateEvent } from './foundation.js';
 import { getTheme } from './themes.js';
