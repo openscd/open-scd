c1833b4b9f108fcb060bbe050450c792ffe5e16c
Jakob Vogelsang <jakob.vogelsang@omicronenergy.com>
2021-12-07 15:36:35 +0100

    chore: move snowpack config to snopack.config file

---
+++ snowpack.config.mjs
@@ -0,0 +1,19 @@
+export default ({
+  plugins: ["@snowpack/plugin-typescript"],
+  packageOptions : {
+    external: ['@web/dev-server-core','@web/dev-server-esbuild','esbuild','crypto'],
+  },
+  exclude: [
+    "**/*.@(spec|test).@(js|mjs)",
+    "**/test/**/*",
+    "**/out-tsc/**/*",
+    "**/.editorconfig",
+    "**/.eslintrc.cjs",
+    "**/.gitignore",
+    "**/.travis.yml",
+    "**/package*",
+    "**/tsconfig.json",
+    "**/web-test-runner.config.mjs",
+    "**/node_modules/**/*"
+  ],  
+});
