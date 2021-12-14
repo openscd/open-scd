8414bd0a9511c034e226013b82214fd366368be8
Jakob Vogelsang <jakob.vogelsang@omicronenergy.com>
2021-12-14 12:30:57 +0100

    test(action-icon): adapt snapshots

--- test/unit/__snapshots__/action-icon.test.snap.js
+++ test/unit/__snapshots__/action-icon.test.snap.js
@@ -2,7 +2,9 @@
 export const snapshots = {};
 
 snapshots["Basic component action-icon with icon property set looks like the latest snapshot"] = 
-`<section>
+`<header>
+</header>
+<section>
   <span>
     <slot name="icon">
     </slot>
@@ -16,7 +18,9 @@ snapshots["Basic component action-icon with icon property set looks like the lat
 /* end snapshot Basic component action-icon with icon property set looks like the latest snapshot */
 
 snapshots["Basic component action-icon with unset icon property  looks like the latest snapshot"] = 
-`<section>
+`<header>
+</header>
+<section>
   <span>
     <slot name="icon">
       <mwc-icon>
