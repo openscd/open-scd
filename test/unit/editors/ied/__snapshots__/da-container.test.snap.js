13c12fb317602643f45fc2e7f3a77e53dc2b658a
Rob Tjalma <rob@tjalma.com>
2022-01-10 14:24:07 +0100

    Added da container unit tests

--- test/unit/editors/ied/__snapshots__/da-container.test.snap.js
+++ test/unit/editors/ied/__snapshots__/da-container.test.snap.js
@@ -1,19 +1,18 @@
 /* @web/test-runner snapshot v1 */
 export const snapshots = {};
 
-snapshots["da-container looks like the latest snapshot with a DA element containing other Enumeration elements and a DAI."] = 
+snapshots["da-container looks like the latest snapshot with a DA element"] = 
 `<action-pane
-  icon="done"
+  icon=""
   tabindex="0"
 >
   <h6>
-    status-only
   </h6>
 </action-pane>
 `;
-/* end snapshot da-container looks like the latest snapshot with a DA element containing other Enumeration elements and a DAI. */
+/* end snapshot da-container looks like the latest snapshot with a DA element */
 
-snapshots["da-container looks like the latest snapshot with a DA element containing other BDA elements."] = 
+snapshots["da-container looks like the latest snapshot with a DA element and child elements are toggled."] = 
 `<action-pane
   icon=""
   tabindex="0"
@@ -25,24 +24,38 @@ snapshots["da-container looks like the latest snapshot with a DA element contain
     <mwc-icon-button-toggle
       id="toggleButton"
       officon="keyboard_arrow_down"
+      on=""
       onicon="keyboard_arrow_up"
     >
     </mwc-icon-button-toggle>
   </abbr>
   <h6>
   </h6>
+  <da-container>
+  </da-container>
+  <da-container>
+  </da-container>
+  <da-container>
+  </da-container>
+  <da-container>
+  </da-container>
+  <da-container>
+  </da-container>
+  <da-container>
+  </da-container>
 </action-pane>
 `;
-/* end snapshot da-container looks like the latest snapshot with a DA element containing other BDA elements. */
+/* end snapshot da-container looks like the latest snapshot with a DA element and child elements are toggled. */
 
-snapshots["da-container looks like the latest snapshot with a DA element containing other Enumeration elements"] = 
+snapshots["da-container looks like the latest snapshot with a DA element containing and a DAI."] = 
 `<action-pane
-  icon=""
+  icon="done"
   tabindex="0"
 >
   <h6>
+    status-only
   </h6>
 </action-pane>
 `;
-/* end snapshot da-container looks like the latest snapshot with a DA element containing other Enumeration elements */
+/* end snapshot da-container looks like the latest snapshot with a DA element containing and a DAI. */
 
