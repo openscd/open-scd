4c3ae59be3ca6ab17ddf0a703b57470f2a413a1a
Jakob Vogelsang <jakob.vogelsang@omicronenergy.com>
2022-01-11 15:43:46 +0100

    feat(wizards/sampledvaluecontrol): add select wizard

---
+++ test/unit/wizards/__snapshots__/sampledvaluecontrol.test.snap.js
@@ -0,0 +1,52 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["Wizards for SCL element SampledValueControl define a select wizard that looks like the latest snapshot"] = 
+`<mwc-dialog
+  defaultaction="close"
+  heading="[wizard.title.select]"
+  open=""
+>
+  <div id="wizard-content">
+    <filtered-list>
+      <mwc-list-item
+        aria-disabled="false"
+        mwc-list-item=""
+        tabindex="-1"
+        twoline=""
+        value="IED2>>CBSW>MSVCB01"
+      >
+        <span>
+          MSVCB01
+        </span>
+        <span slot="secondary">
+          IED2>>CBSW>MSVCB01
+        </span>
+      </mwc-list-item>
+      <mwc-list-item
+        aria-disabled="false"
+        mwc-list-item=""
+        tabindex="-1"
+        twoline=""
+        value="IED3>>MU01>MSVCB01"
+      >
+        <span>
+          MSVCB01
+        </span>
+        <span slot="secondary">
+          IED3>>MU01>MSVCB01
+        </span>
+      </mwc-list-item>
+    </filtered-list>
+  </div>
+  <mwc-button
+    dialogaction="close"
+    label="[cancel]"
+    slot="secondaryAction"
+    style="--mdc-theme-primary: var(--mdc-theme-error)"
+  >
+  </mwc-button>
+</mwc-dialog>
+`;
+/* end snapshot Wizards for SCL element SampledValueControl define a select wizard that looks like the latest snapshot */
+
