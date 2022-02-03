469aa38e036d8a6f8d423fc918c1b9d3e24761b2
cad <cad@sdf.org>
2022-02-03 09:57:40 +0100

    style: center landing icons (#529)
    Make sure to center the landing icons on the page

--- src/open-scd.ts
+++ src/open-scd.ts
@@ -135,9 +135,11 @@ export class OpenSCD extends Hosting(
 
     .landing {
       position: absolute;
+      text-align: center;
       top: 50%;
       left: 50%;
       transform: translate(-50%, -50%);
+      width: 100%;
     }
 
     .landing_icon:hover {
