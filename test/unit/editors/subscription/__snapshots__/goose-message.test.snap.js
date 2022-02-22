d8012fedf166ae4a32b68742c89efca9602e6948
Flurb <private@tjalma.com>
2022-02-22 12:29:31 +0100

    Fixing unit test

--- test/unit/editors/subscription/__snapshots__/goose-message.test.snap.js
+++ test/unit/editors/subscription/__snapshots__/goose-message.test.snap.js
@@ -4,6 +4,7 @@ export const snapshots = {};
 snapshots["goose-message looks like the latest snapshot"] = 
 `<mwc-list-item
   aria-disabled="false"
+  graphic="icon"
   mwc-list-item=""
   tabindex="-1"
 >
