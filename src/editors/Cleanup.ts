c26e451aa44f78e73bb6f79d645b493234e7fa5a
Daniel Mulholland <dan.mulholland@gmail.com>
2022-03-02 19:07:41 +1300

    Implement using mwc-list

--- src/editors/Cleanup.ts
+++ src/editors/Cleanup.ts
@@ -1,28 +1,28 @@
 'use strict';
 
 import { LitElement, html, TemplateResult, property, css } from 'lit-element';
+import { List, MWCListIndex } from '@material/mwc-list';
+import '@material/mwc-list/mwc-check-list-item';
+import '@material/mwc-snackbar';
+
 import { compareNames } from '../foundation.js';
 import {
   Delete,
+  identity,
   newActionEvent,
   newWizardEvent,
+  newSubWizardEvent,
 } from '../foundation.js';
 
-import '@vaadin/grid';
-import '@vaadin/grid/vaadin-grid-column-group.js';
-import '@vaadin/grid/theme/material/vaadin-grid.js';
-import '@vaadin/grid/theme/material/vaadin-grid-filter-column.js';
-import '@vaadin/grid/theme/material/vaadin-grid-selection-column.js';
-
-import type { Grid } from '@vaadin/grid';
+import { editDataSetWizard } from '../wizards/dataset.js';
+import { styles } from './templates/foundation.js';
 
 interface iedData {
-  name: string,
-  type: string,
-  manufacturer: string,
-  dataset: string
+  name: string;
+  type: string;
+  manufacturer: string;
+  dataset: Element;
 }
-
 
 /** An editor [[`plugin`]] for cleaning SCL references and definitions. */
 export default class Cleanup extends LitElement {
@@ -30,7 +30,7 @@ export default class Cleanup extends LitElement {
   @property()
   doc!: XMLDocument;
   @property()
-  areDatasetsCleanable = false;
+  disableDatasetClean = false;
 
   gridRowsUnusedDatasets: iedData[] = [];
 
@@ -64,15 +64,14 @@ export default class Cleanup extends LitElement {
    * @param ied - The SCL IED element to search through.
    * @returns An Array containing the name of all DataSets.
    */
-  private getDatasetNames(ied: Element): string[] {
+  private getDatasetNames(ied: Element): Element[] {
     return Array.from(
       ied.querySelectorAll(
         `:scope > AccessPoint > Server > LDevice > LN0[lnClass="LLN0"] > DataSet`
-      ),
-      elem => elem.getAttribute('name') ?? 'unknown'
+      )
     );
   }
-  
+
   /**
    * Gets DataSets for a particular control block type.
    * @param ied - The SCL IED element to search through.
@@ -115,7 +114,7 @@ export default class Cleanup extends LitElement {
 
   /**
    * Render a user selectable table of unused datasets if any exist, otherwise indicate this is not an issue
-   * @returns 
+   * @returns
    */
   private getUnusedDatasets() {
     // Unused Datasets
@@ -123,9 +122,14 @@ export default class Cleanup extends LitElement {
     this.ieds.forEach(ied => {
       const usedDatasets = this.getUsedDatasets(ied);
       this.getDatasetNames(ied)
-        .filter(ds => !usedDatasets.includes(ds))
+        .filter(
+          ds =>
+            !usedDatasets.includes(
+              ds.getAttribute('name') || 'unknownDataSetName'
+            )
+        )
         .forEach(unusedDS => {
-          const rowItem:iedData = {
+          const rowItem: iedData = {
             name: ied.getAttribute('name') ?? 'unknown',
             type: ied.getAttribute('type') ?? 'unknown',
             manufacturer: ied.getAttribute('manufacturer') ?? 'unknown',
@@ -134,72 +138,92 @@ export default class Cleanup extends LitElement {
           gridRowsUnusedDatasets.push(rowItem);
         });
     });
-    this.gridRowsUnusedDatasets = gridRowsUnusedDatasets;
-    if (this.gridRowsUnusedDatasets.length !== 0) {
-      return html`<h2>Unused Datasets</h2>
-        <section>
-        <vaadin-grid id="grid-datasets" theme="row-stripes">
-          <vaadin-grid-selection-column
-            auto-select
-            frozen
-          ></vaadin-grid-selection-column>
-            <vaadin-grid-filter-column
-            path="name"
-            header="IED Name"
-            ></vaadin-grid-filter-column>
-            <vaadin-grid-filter-column
-            path="type"
-            header="IED Type"
-            ></vaadin-grid-filter-column>
-            <vaadin-grid-filter-column
-            id="manufacturer"
-            path="manufacturer"
-            header="IED Manufacturer"
-            ></vaadin-grid-filter-column>
-          <vaadin-grid-filter-column
-            id="dataset"
-            path="dataset"
-            header="Unused Dataset Name"
-          ></vaadin-grid-filter-column>
-          </vaadin-grid-column-group>
-        </vaadin-grid>
-        <mwc-button
-              icon="delete" id="grid-datasets-delete"
-              label="Remove Selected Datasets"
-              ?disabled=${!this.areDatasetsCleanable}
-              @click=${(e: MouseEvent) => {
-                const deleteActions = this.cleanDatasets();
-                deleteActions.forEach(deleteAction =>
-                  e.target?.dispatchEvent(newActionEvent(deleteAction))
-                );
-                e.target?.dispatchEvent(newWizardEvent());
-                this.render();
-              }}
-              slot="secondaryAction"
-            ></mwc-button>
-        </section>`;
-    } else {
-      return html`<h2>Datasets</h2>
-        <p>✓ All existing datasets are assigned</p> `;
-    }
+    this.gridRowsUnusedDatasets = gridRowsUnusedDatasets.sort((a, b) =>
+      a.dataset
+        .getAttribute('name')!
+        .localeCompare(b.dataset.getAttribute('name')!)
+    );
+    const thing = html`
+      <h1>
+        Unused Datasets
+        <abbr slot="action">
+          <mwc-icon-button
+            icon="info"
+            title="Datasets without an associated Sampled Value, Report or GOOSE control block."
+          >
+          </mwc-icon-button>
+        </abbr>
+      </h1>
+      <filtered-list multi id="unuseddatasetlist"
+        >${Array.from(
+          this.gridRowsUnusedDatasets.map(
+            item =>
+              html`<mwc-check-list-item
+                twoline
+                value="${identity(item.dataset)}"
+                ><span class="datasetname"
+                  >${item.dataset.getAttribute('name')}
+                </span>
+                <span>
+                  <mwc-icon-button
+                    label="Edit"
+                    icon="edit"
+                    class="editunuseddataset"
+                    @click=${(e: MouseEvent) => {
+                      e.target?.dispatchEvent(
+                        newSubWizardEvent(() => editDataSetWizard(item.dataset))
+                      );
+                    }}
+                  ></mwc-icon-button>
+                </span>
+                <span slot="secondary"
+                  >${item.name} (${item.manufacturer}) - ${item.type}</span
+                >
+              </mwc-check-list-item>`
+          )
+        )}
+      </filtered-list>
+      <mwc-button
+        icon="delete"
+        id="grid-datasets-delete"
+        label="Remove Selected Datasets"
+        ?disabled=${this.disableDatasetClean}
+        slot="secondaryAction"
+        @click=${(e: MouseEvent) => {
+          const selectedItems = <MWCListIndex>(
+            (<List>this.shadowRoot!.querySelector('#unuseddatasetlist')).index
+          );
+          const cleanItems = Array.from(selectedItems.values()).map(
+            index => this.gridRowsUnusedDatasets[index]
+          );
+          const deleteActions = this.cleanDatasets(cleanItems);
+          deleteActions.forEach(deleteAction =>
+            e.target?.dispatchEvent(newActionEvent(deleteAction))
+          );
+          e.target?.dispatchEvent(newWizardEvent());
+        }}
+      ></mwc-button>
+    `;
+    return thing;
   }
 
   /**
    * Clean datasets as requested by removing DataSet elements specified by the user from the SCL file
    * @returns an actions array to support undo/redo
    */
-   private cleanDatasets(): Delete[] {
-    const gridDataset: Grid|null|undefined = this.shadowRoot?.querySelector('#grid-datasets');
+  private cleanDatasets(cleanItems: iedData[]): Delete[] {
     const actions: Delete[] = [];
-    if (gridDataset) {
-      gridDataset.selectedItems.forEach(item => {
-        const itemToDelete: Element = this.getDatasetFromIED(item.name, item.dataset)!;
-        console.log(itemToDelete);
+    if (cleanItems) {
+      cleanItems.forEach(item => {
+        const itemToDelete: Element = this.getDatasetFromIED(
+          item.name,
+          item.dataset.getAttribute('name')!
+        )!;
         actions.push({
           old: {
-            parent: <Element>(itemToDelete.parentElement!),
+            parent: <Element>itemToDelete.parentElement!,
             element: itemToDelete,
-            reference: <Node|null>itemToDelete!.nextSibling,
+            reference: <Node | null>itemToDelete!.nextSibling,
           },
         });
       });
@@ -208,78 +232,44 @@ export default class Cleanup extends LitElement {
   }
 
   async firstUpdated(): Promise<void> {
-    const gridDataset: Grid|null|undefined = this.shadowRoot?.querySelector('#grid-datasets');
-    // add event listener to allow button to be disabled
-    if (gridDataset) {
-      gridDataset.items = this.gridRowsUnusedDatasets;
-      gridDataset.addEventListener('selected-items-changed', event => {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const selectedItems:any|null = event.target
-        this.areDatasetsCleanable = selectedItems?.length !== 0
-      });
-    }
+    const checklist: List =
+      this.shadowRoot!.querySelector('#unuseddatasetlist')!;
+    checklist!.addEventListener('selected', () => {
+      this.disableDatasetClean = checklist!.index.size === 0;
+    });
   }
 
   render(): TemplateResult {
-    return html` <div id="containerTemplates">
-      <section>${this.getUnusedDatasets()}</section>
-    </div>`;
+    return html`
+      <div id="containerTemplates">
+        <section tabindex="0">${this.getUnusedDatasets()}</section>
+      </div>
+    `;
   }
 
   static styles = css`
-    h2 {
-      color: var(--base00);
-      font-family: 'Roboto', sans-serif;
-      font-weight: 300;
-      overflow: hidden;
-      white-space: nowrap;
-      text-overflow: ellipsis;
-      margin: 0px;
-      line-height: 48px;
-      padding-left: 0.3em;
+    ${styles}
+
+    :host {
+      width: 100vw;
     }
 
-    p {
-      color: var(--base00);
-      font-family: Roboto, sans-serif;
-      font-weight: 300;
-      overflow: clip visible;
-      white-space: nowrap;
-      text-overflow: ellipsis;
-      margin: 0px;
-      line-height: 52px;
-      padding-left: 1rem;
+    #containerTemplates {
+      display: grid;
+      grid-gap: 12px;
+      padding: 8px 12px 16px;
+      box-sizing: border-box;
+      grid-template-columns: repeat(auto-fit, minmax(316px, auto));
     }
 
-    vaadin-grid {
-      margin-top: 0em;
+    @media (max-width: 387px) {
+      #containerTemplates {
+        grid-template-columns: repeat(auto-fit, minmax(196px, auto));
+      }
     }
 
-    vaadin-grid-filter-column {
-      width: 3em;
-    }
-
-    mwc-button {
-      margin: 0.25rem;
-      margin-left: 1rem;
-    }
-
-    #grid-datasets {
-      width: auto;
-      margin-left: 1rem;
-      margin-right: 2rem;
-      height: 400px;
-    }
-
-    /* matching vaadin-grid material themes with open-scd themes */
-    * {
-      --material-body-text-color: var(--base00);
-      --material-secondary-text-color: var(--base00);
-      --material-primary-text-color: var(--base01);
-      --material-error-text-color: var(--red);
-      --material-primary-color: var(--primary);
-      --material-error-color: var(--red);
-      --material-background-color: var(--base3);
+    .editunuseddataset {
+      --mdc-icon-size: 16px;
     }
   `;
 }
