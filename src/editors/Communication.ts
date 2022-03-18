b8e04a9a1e73cfe2d253006780d2a2a6149e060f
danyill <danyill@users.noreply.github.com>
2022-03-18 21:44:18 +1300

    refactor(plugins/Communication): Add styling for Communication section

--- src/editors/Communication.ts
+++ src/editors/Communication.ts
@@ -73,6 +73,19 @@ export default class CommunicationPlugin extends LitElement {
       width: 100vw;
     }
 
+    h1 {
+      color: var(--mdc-theme-on-surface);
+      font-family: 'Roboto', sans-serif;
+      font-weight: 300;
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      margin: 0px;
+      line-height: 48px;
+      padding-left: 0.3em;
+      transition: background-color 150ms linear;
+    }
+
     section {
       outline: none;
       padding: 8px 12px 16px;
