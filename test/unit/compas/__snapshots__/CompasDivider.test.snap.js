6ff7b5538117a8a46c566f965c19064a87820a3f
Dennis Labordus <dennis.labordus@alliander.com>
2022-03-21 08:36:07 +0100

    Replace Compas Divider with Wizard Divider.
    Signed-off-by: Dennis Labordus <dennis.labordus@alliander.com>

--- test/unit/compas/__snapshots__/CompasDivider.test.snap.js
+++
@@ -1,9 +0,0 @@
-/* @web/test-runner snapshot v1 */
-export const snapshots = {};
-
-snapshots["compas-divider looks like the latest snapshot"] = 
-`<div role="separator">
-</div>
-`;
-/* end snapshot compas-divider looks like the latest snapshot */
-
