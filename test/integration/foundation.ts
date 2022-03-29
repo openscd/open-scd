ac395e07f4759dd28a688e0d5b723c6986d61633
Dennis Labordus <dennis.labordus@alliander.com>
2022-03-29 13:14:33 +0200

    Fixed (snapshot) tests.
    Signed-off-by: Dennis Labordus <dennis.labordus@alliander.com>

--- test/integration/foundation.ts
+++
@@ -1,7 +0,0 @@
-export function cleanLocalStorageForNsdocFiles(): void {
-  // Cleanup NSDoc Files from LocalStorage
-  localStorage.removeItem('IEC 61850-7-2');
-  localStorage.removeItem('IEC 61850-7-3');
-  localStorage.removeItem('IEC 61850-7-4');
-  localStorage.removeItem('IEC 61850-8-1');
-}
