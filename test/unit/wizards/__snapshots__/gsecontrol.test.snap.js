d4bdf47ed206303b23312879cda1bf0b01670776
Jakob Vogelsang <jakob-vogelsang@posteo.de>
2022-03-31 23:06:40 +0200

    test(wizards/gsecontrol): add unit tests

--- test/unit/wizards/__snapshots__/gsecontrol.test.snap.js
+++ test/unit/wizards/__snapshots__/gsecontrol.test.snap.js
@@ -329,3 +329,264 @@ snapshots["gsecontrol wizards editGseControlWizard looks like the latest snapsho
 `;
 /* end snapshot gsecontrol wizards editGseControlWizard looks like the latest snapshot */
 
+snapshots["gsecontrol wizards define an create wizard that with existing ConnectedAP element in the Communication section the first page looks like the latest snapshot"] = 
+`<mwc-dialog
+  defaultaction="close"
+  heading="[wizard.title.add]"
+  open=""
+>
+  <div id="wizard-content">
+    <wizard-textfield
+      dialoginitialfocus=""
+      helper="[scl.name]"
+      label="name"
+      maxlength="32"
+      pattern="[A-Za-z][0-9,A-Z,a-z_]*"
+      required=""
+      validationmessage="[textfield.required]"
+    >
+    </wizard-textfield>
+    <wizard-textfield
+      disabled=""
+      helper="[scl.desc]"
+      label="desc"
+      nullable=""
+    >
+    </wizard-textfield>
+    <wizard-select
+      helper="[scl.type]"
+      label="type"
+      nullable=""
+      required=""
+    >
+      <mwc-list-item
+        aria-disabled="false"
+        mwc-list-item=""
+        role="option"
+        tabindex="-1"
+        value="GOOSE"
+      >
+        GOOSE
+      </mwc-list-item>
+      <mwc-list-item
+        aria-disabled="false"
+        mwc-list-item=""
+        role="option"
+        tabindex="-1"
+        value="GSSE"
+      >
+        GSSE
+      </mwc-list-item>
+    </wizard-select>
+    <wizard-textfield
+      helper="[scl.id]"
+      label="appID"
+      required=""
+      validationmessage="[textfield.nonempty]"
+    >
+    </wizard-textfield>
+    <wizard-checkbox
+      helper="[scl.fixedOffs]"
+      label="fixedOffs"
+      nullable=""
+    >
+    </wizard-checkbox>
+    <wizard-select
+      disabled=""
+      helper="[scl.securityEnable]"
+      label="securityEnabled"
+      nullable=""
+      required=""
+    >
+      <mwc-list-item
+        aria-disabled="false"
+        mwc-list-item=""
+        role="option"
+        tabindex="-1"
+        value="None"
+      >
+        None
+      </mwc-list-item>
+      <mwc-list-item
+        aria-disabled="false"
+        mwc-list-item=""
+        role="option"
+        tabindex="-1"
+        value="Signature"
+      >
+        Signature
+      </mwc-list-item>
+      <mwc-list-item
+        aria-disabled="false"
+        mwc-list-item=""
+        role="option"
+        tabindex="-1"
+        value="SignatureAndEncryption"
+      >
+        SignatureAndEncryption
+      </mwc-list-item>
+    </wizard-select>
+  </div>
+  <mwc-button
+    dialogaction="close"
+    label="[cancel]"
+    slot="secondaryAction"
+    style="--mdc-theme-primary: var(--mdc-theme-error)"
+  >
+  </mwc-button>
+  <mwc-button
+    dialogaction="next"
+    icon="navigate_next"
+    label="[wizard.title.add]"
+    slot="primaryAction"
+    trailingicon=""
+  >
+  </mwc-button>
+</mwc-dialog>
+`;
+/* end snapshot gsecontrol wizards define an create wizard that with existing ConnectedAP element in the Communication section the first page looks like the latest snapshot */
+
+snapshots["gsecontrol wizards define an create wizard that with existing ConnectedAP element in the Communication section the second page looks like the latest snapshot"] = 
+`<mwc-dialog
+  defaultaction="close"
+  heading="[wizard.title.add]"
+>
+  <div id="wizard-content">
+    <mwc-formfield label="[connectedap.wizard.addschemainsttype]">
+      <mwc-checkbox
+        checked=""
+        id="instType"
+      >
+      </mwc-checkbox>
+    </mwc-formfield>
+    <wizard-textfield
+      label="MAC-Address"
+      pattern="([0-9A-F]{2}-){5}[0-9A-F]{2}"
+      required=""
+    >
+    </wizard-textfield>
+    <wizard-textfield
+      label="APPID"
+      pattern="[0-9A-F]{4}"
+      required=""
+    >
+    </wizard-textfield>
+    <wizard-textfield
+      label="VLAN-ID"
+      nullable=""
+      pattern="[0-9A-F]{3}"
+      required=""
+    >
+    </wizard-textfield>
+    <wizard-textfield
+      label="VLAN-PRIORITY"
+      nullable=""
+      pattern="[0-7]"
+      required=""
+    >
+    </wizard-textfield>
+    <wizard-textfield
+      label="MinTime"
+      nullable=""
+      suffix="ms"
+      type="number"
+    >
+    </wizard-textfield>
+    <wizard-textfield
+      label="MaxTime"
+      nullable=""
+      suffix="ms"
+      type="number"
+    >
+    </wizard-textfield>
+  </div>
+  <mwc-button
+    dialogaction="prev"
+    icon="navigate_before"
+    label="[wizard.title.add]"
+    slot="secondaryAction"
+  >
+  </mwc-button>
+  <mwc-button
+    dialogaction="close"
+    label="[cancel]"
+    slot="secondaryAction"
+    style="--mdc-theme-primary: var(--mdc-theme-error)"
+  >
+  </mwc-button>
+  <mwc-button
+    dialogaction="next"
+    icon="navigate_next"
+    label="[dataset.fcda.add]"
+    slot="primaryAction"
+    trailingicon=""
+  >
+  </mwc-button>
+</mwc-dialog>
+`;
+/* end snapshot gsecontrol wizards define an create wizard that with existing ConnectedAP element in the Communication section the second page looks like the latest snapshot */
+
+snapshots["gsecontrol wizards define an create wizard that with existing ConnectedAP element in the Communication section the third page looks like the latest snapshot"] = 
+`<mwc-dialog
+  defaultaction="close"
+  heading="[dataset.fcda.add]"
+>
+  <div id="wizard-content">
+    <finder-list multi="">
+    </finder-list>
+  </div>
+  <mwc-button
+    dialogaction="prev"
+    icon="navigate_before"
+    label="[wizard.title.add]"
+    slot="secondaryAction"
+  >
+  </mwc-button>
+  <mwc-button
+    dialogaction="close"
+    label="[cancel]"
+    slot="secondaryAction"
+    style="--mdc-theme-primary: var(--mdc-theme-error)"
+  >
+  </mwc-button>
+  <mwc-button
+    dialoginitialfocus=""
+    icon="save"
+    label="[save]"
+    slot="primaryAction"
+    trailingicon=""
+  >
+  </mwc-button>
+</mwc-dialog>
+`;
+/* end snapshot gsecontrol wizards define an create wizard that with existing ConnectedAP element in the Communication section the third page looks like the latest snapshot */
+
+snapshots["gsecontrol wizards define a wizard to select the control block reference looks like the latest snapshot"] = 
+`<mwc-dialog
+  defaultaction="close"
+  heading="[report.wizard.location]"
+  open=""
+>
+  <div id="wizard-content">
+    <finder-list path="[&quot;SCL: &quot;]">
+    </finder-list>
+  </div>
+  <mwc-button
+    dialogaction="close"
+    label="[cancel]"
+    slot="secondaryAction"
+    style="--mdc-theme-primary: var(--mdc-theme-error)"
+  >
+  </mwc-button>
+  <mwc-button
+    dialoginitialfocus=""
+    icon=""
+    label="[next]"
+    slot="primaryAction"
+    trailingicon=""
+  >
+  </mwc-button>
+</mwc-dialog>
+`;
+/* end snapshot gsecontrol wizards define a wizard to select the control block reference looks like the latest snapshot */
+
--- test/unit/wizards/gsecontrol.test.ts
+++ test/unit/wizards/gsecontrol.test.ts
@@ -1,4 +1,5 @@
 import { expect, fixture, html } from '@open-wc/testing';
+import { SinonSpy, spy } from 'sinon';
 import fc from 'fast-check';
 
 import '../../mock-wizard.js';
@@ -6,9 +7,13 @@ import { MockWizard } from '../../mock-wizard.js';
 
 import { WizardTextField } from '../../../src/wizard-textfield.js';
 import {
+  ComplexAction,
+  Create,
   Delete,
+  isCreate,
   isDelete,
   isReplace,
+  isSimple,
   Replace,
   Wizard,
   WizardInputElement,
@@ -19,18 +24,28 @@ import {
   contentGseControlWizard,
   selectGseControlWizard,
   updateGseControlAction,
+  gseControlParentSelector,
+  createGseControlWizard,
 } from '../../../src/wizards/gsecontrol.js';
 import { regExp, regexString } from '../../foundation.js';
+import { FinderList } from '../../../src/finder-list.js';
 
 describe('gsecontrol wizards', () => {
   let doc: XMLDocument;
   let element: MockWizard;
+
+  let primaryAction: HTMLElement;
+
+  let actionEvent: SinonSpy;
 
   beforeEach(async () => {
     element = await fixture(html`<mock-wizard></mock-wizard>`);
     doc = await fetch('/test/testfiles/wizards/gsecontrol.scd')
       .then(response => response.text())
       .then(str => new DOMParser().parseFromString(str, 'application/xml'));
+
+    actionEvent = spy();
+    window.addEventListener('editor-action', actionEvent);
   });
 
   describe('selectGseControlWizard', () => {
@@ -340,5 +355,205 @@ describe('gsecontrol wizards', () => {
         'SignatureAndEncryption'
       );
     });
+  });
+
+  describe('define an create wizard that', () => {
+    let dataPicker: FinderList;
+
+    describe('with existing ConnectedAP element in the Communication section', () => {
+      beforeEach(async () => {
+        const wizard = createGseControlWizard(doc.querySelector('LN0')!);
+        element.workflow.push(() => wizard);
+        await element.requestUpdate();
+
+        (<WizardTextField>(
+          element.wizardUI.dialogs[0].querySelector(
+            'wizard-textfield[label="appID"]'
+          )
+        )).maybeValue = 'wer';
+
+        primaryAction = <HTMLElement>(
+          element.wizardUI.dialogs[2]?.querySelector(
+            'mwc-button[slot="primaryAction"]'
+          )
+        );
+
+        dataPicker = <FinderList>(
+          element.wizardUI.dialogs[2]?.querySelector('finder-list')
+        );
+
+        await element.wizardUI.requestUpdate(); // make sure wizard is rendered
+      });
+
+      it('has three pages', () =>
+        expect(element.wizardUI.dialogs.length).to.equal(3));
+
+      it('the first page looks like the latest snapshot', async () => {
+        await expect(element.wizardUI.dialogs[0]).dom.to.equalSnapshot();
+      }).timeout(5000);
+
+      it('the second page looks like the latest snapshot', async () => {
+        await expect(element.wizardUI.dialogs[1]).dom.to.equalSnapshot();
+      }).timeout(5000);
+
+      it('the third page looks like the latest snapshot', async () => {
+        await expect(element.wizardUI.dialogs[2]).dom.to.equalSnapshot();
+      }).timeout(5000);
+
+      it('triggers complex action on primary action click', async () => {
+        await primaryAction.click();
+
+        expect(actionEvent).to.be.calledOnce;
+        const action = actionEvent.args[0][0].detail.action;
+        expect(action).to.not.satisfy(isSimple);
+      });
+
+      it('complex action carries GSEControl element', async () => {
+        await primaryAction.click();
+
+        const actions = (<ComplexAction>actionEvent.args[0][0].detail.action)
+          .actions;
+        expect(actions.length).to.equal(3);
+        const action = actions[0];
+        expect(action).to.satisfy(isCreate);
+        const createAction = <Create>action;
+        expect((<Element>createAction.new.element).tagName).to.equal(
+          'GSEControl'
+        );
+      });
+
+      it('add default confRev to the GSEControl element', async () => {
+        await primaryAction.click();
+
+        const actions = (<ComplexAction>actionEvent.args[0][0].detail.action)
+          .actions;
+        expect(actions.length).to.equal(3);
+        const action = actions[0];
+        expect(action).to.satisfy(isCreate);
+        const createAction = <Create>action;
+        expect(createAction.new.element).has.attribute('confRev', '1');
+      });
+
+      it('complex action carries referenced DataSet element', async () => {
+        await primaryAction.click();
+
+        const actions = (<ComplexAction>actionEvent.args[0][0].detail.action)
+          .actions;
+        expect(actions.length).to.equal(3);
+        const action = actions[2];
+        expect(action).to.satisfy(isCreate);
+        const createAction = <Create>action;
+        expect((<Element>createAction.new.element).tagName).to.equal('DataSet');
+      });
+
+      it('referenced DataSet element not having any FCDA per default', async () => {
+        await primaryAction.click();
+
+        const createAction = <Create>(
+          (<ComplexAction>actionEvent.args[0][0].detail.action).actions[2]
+        );
+        expect((<Element>createAction.new.element).children).to.be.empty;
+      });
+
+      it('referenced DataSet element saving selected FCDA', async () => {
+        const path = [
+          'Server: IED2>P1',
+          'LDevice: IED2>>CircuitBreaker_CB1',
+          'LN0: IED2>>CircuitBreaker_CB1',
+          'DO: #Dummy.LLN0>Beh',
+          'DA: #Dummy.LLN0.Beh>stVal',
+        ];
+
+        dataPicker.paths = [path];
+        await element.requestUpdate();
+
+        await primaryAction.click();
+
+        const createAction = <Create>(
+          (<ComplexAction>actionEvent.args[0][0].detail.action).actions[2]
+        );
+        expect((<Element>createAction.new.element).children).to.not.be.empty;
+        expect((<Element>createAction.new.element).children).to.have.lengthOf(
+          1
+        );
+      });
+
+      it('complex action adding GSE element in the Communication section', async () => {
+        await primaryAction.click();
+
+        const actions = (<ComplexAction>actionEvent.args[0][0].detail.action)
+          .actions;
+        expect(actions.length).to.equal(3);
+        const action = actions[1];
+        expect(action).to.satisfy(isCreate);
+        const createAction = <Create>action;
+        expect((<Element>createAction.new.element).tagName).to.equal('GSE');
+      });
+    });
+
+    describe('with missing ConnectedAP element in the Communication section', () => {
+      beforeEach(async () => {
+        const wizard = createGseControlWizard(
+          doc.querySelector('IED[name="IED4"] LN0')!
+        );
+        element.workflow.push(() => wizard);
+        await element.requestUpdate();
+
+        (<WizardTextField>(
+          element.wizardUI.dialogs[0].querySelector(
+            'wizard-textfield[label="appID"]'
+          )
+        )).maybeValue = 'wer';
+
+        primaryAction = <HTMLElement>(
+          element.wizardUI.dialogs[1]?.querySelector(
+            'mwc-button[slot="primaryAction"]'
+          )
+        );
+
+        dataPicker = <FinderList>(
+          element.wizardUI.dialogs[2]?.querySelector('finder-list')
+        );
+
+        await element.wizardUI.requestUpdate(); // make sure wizard is rendered
+      });
+
+      it('has two pages', () =>
+        expect(element.wizardUI.dialogs.length).to.equal(2));
+
+      it('triggers complex action on primary action click', async () => {
+        await primaryAction.click();
+
+        expect(actionEvent).to.be.calledOnce;
+        const action = actionEvent.args[0][0].detail.action;
+        expect(action).to.not.satisfy(isSimple);
+      });
+
+      it('complex action NOT adding GSE element in the Communication section', async () => {
+        await primaryAction.click();
+
+        const actions = (<ComplexAction>actionEvent.args[0][0].detail.action)
+          .actions;
+        expect(actions.length).to.equal(2);
+        const action = actions[1];
+        expect(action).to.satisfy(isCreate);
+        const createAction = <Create>action;
+        expect((<Element>createAction.new.element).tagName).to.equal('DataSet');
+      });
+    });
+  });
+
+  describe('define a wizard to select the control block reference', () => {
+    beforeEach(async () => {
+      const wizard = gseControlParentSelector(doc);
+      element.workflow.push(() => wizard);
+      await element.requestUpdate();
+
+      await element.wizardUI.requestUpdate(); // make sure wizard is rendered
+    });
+
+    it('looks like the latest snapshot', async () => {
+      await expect(element.wizardUI.dialog).dom.to.equalSnapshot();
+    }).timeout(5000);
   });
 });
