e3625f731d0f08fc9c7776e3f9527955f8a31885
Flurb <private@tjalma.com>
2022-04-13 09:25:06 +0200

    Added unit tests

---
+++ test/unit/editors/ied/foundation/__snapshots__/foundation.test.snap.js
@@ -0,0 +1,612 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["foundation getCustomField renders a INT8 field correctly"] = 
+`<div style="display: flex; flex-direction: row;">
+  <div style="flex: auto;">
+    <label class="mdc-text-field mdc-text-field--disabled mdc-text-field--filled mdc-text-field--no-label">
+      <span class="mdc-text-field__ripple">
+      </span>
+      <input
+        class="mdc-text-field__input"
+        disabled=""
+        max="255"
+        min="-256"
+        placeholder=""
+        type="number"
+      >
+      <span class="mdc-line-ripple">
+      </span>
+    </label>
+  </div>
+  <div style="display: flex; align-items: center; height: 56px;">
+    <mwc-icon-button
+      icon="done"
+      id="doneButton"
+      style="color: var(--mdc-theme-on-surface); display: none;"
+    >
+    </mwc-icon-button>
+    <mwc-icon-button-toggle
+      officon="edit"
+      onicon="clear"
+      style="margin-left: 5px; color: var(--mdc-theme-on-surface);"
+    >
+    </mwc-icon-button-toggle>
+  </div>
+</div>
+`;
+/* end snapshot foundation getCustomField renders a INT8 field correctly */
+
+snapshots["foundation getCustomField renders a INT16 field correctly"] = 
+`<div style="display: flex; flex-direction: row;">
+  <div style="flex: auto;">
+    <label class="mdc-text-field mdc-text-field--disabled mdc-text-field--filled mdc-text-field--no-label">
+      <span class="mdc-text-field__ripple">
+      </span>
+      <input
+        class="mdc-text-field__input"
+        disabled=""
+        max="65535"
+        min="-65536"
+        placeholder=""
+        type="number"
+      >
+      <span class="mdc-line-ripple">
+      </span>
+    </label>
+  </div>
+  <div style="display: flex; align-items: center; height: 56px;">
+    <mwc-icon-button
+      icon="done"
+      id="doneButton"
+      style="color: var(--mdc-theme-on-surface); display: none;"
+    >
+    </mwc-icon-button>
+    <mwc-icon-button-toggle
+      officon="edit"
+      onicon="clear"
+      style="margin-left: 5px; color: var(--mdc-theme-on-surface);"
+    >
+    </mwc-icon-button-toggle>
+  </div>
+</div>
+`;
+/* end snapshot foundation getCustomField renders a INT16 field correctly */
+
+snapshots["foundation getCustomField renders a INT24 field correctly"] = 
+`<div style="display: flex; flex-direction: row;">
+  <div style="flex: auto;">
+    <label class="mdc-text-field mdc-text-field--disabled mdc-text-field--filled mdc-text-field--no-label">
+      <span class="mdc-text-field__ripple">
+      </span>
+      <input
+        class="mdc-text-field__input"
+        disabled=""
+        max="16777215"
+        min="-16777216"
+        placeholder=""
+        type="number"
+      >
+      <span class="mdc-line-ripple">
+      </span>
+    </label>
+  </div>
+  <div style="display: flex; align-items: center; height: 56px;">
+    <mwc-icon-button
+      icon="done"
+      id="doneButton"
+      style="color: var(--mdc-theme-on-surface); display: none;"
+    >
+    </mwc-icon-button>
+    <mwc-icon-button-toggle
+      officon="edit"
+      onicon="clear"
+      style="margin-left: 5px; color: var(--mdc-theme-on-surface);"
+    >
+    </mwc-icon-button-toggle>
+  </div>
+</div>
+`;
+/* end snapshot foundation getCustomField renders a INT24 field correctly */
+
+snapshots["foundation getCustomField renders a INT32 field correctly"] = 
+`<div style="display: flex; flex-direction: row;">
+  <div style="flex: auto;">
+    <label class="mdc-text-field mdc-text-field--disabled mdc-text-field--filled mdc-text-field--no-label">
+      <span class="mdc-text-field__ripple">
+      </span>
+      <input
+        class="mdc-text-field__input"
+        disabled=""
+        max="4294967295"
+        min="-4294967296"
+        placeholder=""
+        type="number"
+      >
+      <span class="mdc-line-ripple">
+      </span>
+    </label>
+  </div>
+  <div style="display: flex; align-items: center; height: 56px;">
+    <mwc-icon-button
+      icon="done"
+      id="doneButton"
+      style="color: var(--mdc-theme-on-surface); display: none;"
+    >
+    </mwc-icon-button>
+    <mwc-icon-button-toggle
+      officon="edit"
+      onicon="clear"
+      style="margin-left: 5px; color: var(--mdc-theme-on-surface);"
+    >
+    </mwc-icon-button-toggle>
+  </div>
+</div>
+`;
+/* end snapshot foundation getCustomField renders a INT32 field correctly */
+
+snapshots["foundation getCustomField renders a INT64 field correctly"] = 
+`<div style="display: flex; flex-direction: row;">
+  <div style="flex: auto;">
+    <label class="mdc-text-field mdc-text-field--disabled mdc-text-field--filled mdc-text-field--no-label">
+      <span class="mdc-text-field__ripple">
+      </span>
+      <input
+        class="mdc-text-field__input"
+        disabled=""
+        max="18446744073709552000"
+        min="-18446744073709552000"
+        placeholder=""
+        type="number"
+      >
+      <span class="mdc-line-ripple">
+      </span>
+    </label>
+  </div>
+  <div style="display: flex; align-items: center; height: 56px;">
+    <mwc-icon-button
+      icon="done"
+      id="doneButton"
+      style="color: var(--mdc-theme-on-surface); display: none;"
+    >
+    </mwc-icon-button>
+    <mwc-icon-button-toggle
+      officon="edit"
+      onicon="clear"
+      style="margin-left: 5px; color: var(--mdc-theme-on-surface);"
+    >
+    </mwc-icon-button-toggle>
+  </div>
+</div>
+`;
+/* end snapshot foundation getCustomField renders a INT64 field correctly */
+
+snapshots["foundation getCustomField renders a INT128 field correctly"] = 
+`<div style="display: flex; flex-direction: row;">
+  <div style="flex: auto;">
+    <label class="mdc-text-field mdc-text-field--disabled mdc-text-field--filled mdc-text-field--no-label">
+      <span class="mdc-text-field__ripple">
+      </span>
+      <input
+        class="mdc-text-field__input"
+        disabled=""
+        max="3.402823669209385e+38"
+        min="-3.402823669209385e+38"
+        placeholder=""
+        type="number"
+      >
+      <span class="mdc-line-ripple">
+      </span>
+    </label>
+  </div>
+  <div style="display: flex; align-items: center; height: 56px;">
+    <mwc-icon-button
+      icon="done"
+      id="doneButton"
+      style="color: var(--mdc-theme-on-surface); display: none;"
+    >
+    </mwc-icon-button>
+    <mwc-icon-button-toggle
+      officon="edit"
+      onicon="clear"
+      style="margin-left: 5px; color: var(--mdc-theme-on-surface);"
+    >
+    </mwc-icon-button-toggle>
+  </div>
+</div>
+`;
+/* end snapshot foundation getCustomField renders a INT128 field correctly */
+
+snapshots["foundation getCustomField renders a INT8U field correctly"] = 
+`<div style="display: flex; flex-direction: row;">
+  <div style="flex: auto;">
+    <label class="mdc-text-field mdc-text-field--disabled mdc-text-field--filled mdc-text-field--no-label">
+      <span class="mdc-text-field__ripple">
+      </span>
+      <input
+        class="mdc-text-field__input"
+        disabled=""
+        max="255"
+        min="0"
+        placeholder=""
+        type="number"
+      >
+      <span class="mdc-line-ripple">
+      </span>
+    </label>
+  </div>
+  <div style="display: flex; align-items: center; height: 56px;">
+    <mwc-icon-button
+      icon="done"
+      id="doneButton"
+      style="color: var(--mdc-theme-on-surface); display: none;"
+    >
+    </mwc-icon-button>
+    <mwc-icon-button-toggle
+      officon="edit"
+      onicon="clear"
+      style="margin-left: 5px; color: var(--mdc-theme-on-surface);"
+    >
+    </mwc-icon-button-toggle>
+  </div>
+</div>
+`;
+/* end snapshot foundation getCustomField renders a INT8U field correctly */
+
+snapshots["foundation getCustomField renders a INT16U field correctly"] = 
+`<div style="display: flex; flex-direction: row;">
+  <div style="flex: auto;">
+    <label class="mdc-text-field mdc-text-field--disabled mdc-text-field--filled mdc-text-field--no-label">
+      <span class="mdc-text-field__ripple">
+      </span>
+      <input
+        class="mdc-text-field__input"
+        disabled=""
+        max="65535"
+        min="0"
+        placeholder=""
+        type="number"
+      >
+      <span class="mdc-line-ripple">
+      </span>
+    </label>
+  </div>
+  <div style="display: flex; align-items: center; height: 56px;">
+    <mwc-icon-button
+      icon="done"
+      id="doneButton"
+      style="color: var(--mdc-theme-on-surface); display: none;"
+    >
+    </mwc-icon-button>
+    <mwc-icon-button-toggle
+      officon="edit"
+      onicon="clear"
+      style="margin-left: 5px; color: var(--mdc-theme-on-surface);"
+    >
+    </mwc-icon-button-toggle>
+  </div>
+</div>
+`;
+/* end snapshot foundation getCustomField renders a INT16U field correctly */
+
+snapshots["foundation getCustomField renders a INT24U field correctly"] = 
+`<div style="display: flex; flex-direction: row;">
+  <div style="flex: auto;">
+    <label class="mdc-text-field mdc-text-field--disabled mdc-text-field--filled mdc-text-field--no-label">
+      <span class="mdc-text-field__ripple">
+      </span>
+      <input
+        class="mdc-text-field__input"
+        disabled=""
+        max="16777215"
+        min="0"
+        placeholder=""
+        type="number"
+      >
+      <span class="mdc-line-ripple">
+      </span>
+    </label>
+  </div>
+  <div style="display: flex; align-items: center; height: 56px;">
+    <mwc-icon-button
+      icon="done"
+      id="doneButton"
+      style="color: var(--mdc-theme-on-surface); display: none;"
+    >
+    </mwc-icon-button>
+    <mwc-icon-button-toggle
+      officon="edit"
+      onicon="clear"
+      style="margin-left: 5px; color: var(--mdc-theme-on-surface);"
+    >
+    </mwc-icon-button-toggle>
+  </div>
+</div>
+`;
+/* end snapshot foundation getCustomField renders a INT24U field correctly */
+
+snapshots["foundation getCustomField renders a INT32U field correctly"] = 
+`<div style="display: flex; flex-direction: row;">
+  <div style="flex: auto;">
+    <label class="mdc-text-field mdc-text-field--disabled mdc-text-field--filled mdc-text-field--no-label">
+      <span class="mdc-text-field__ripple">
+      </span>
+      <input
+        class="mdc-text-field__input"
+        disabled=""
+        max="4294967295"
+        min="0"
+        placeholder=""
+        type="number"
+      >
+      <span class="mdc-line-ripple">
+      </span>
+    </label>
+  </div>
+  <div style="display: flex; align-items: center; height: 56px;">
+    <mwc-icon-button
+      icon="done"
+      id="doneButton"
+      style="color: var(--mdc-theme-on-surface); display: none;"
+    >
+    </mwc-icon-button>
+    <mwc-icon-button-toggle
+      officon="edit"
+      onicon="clear"
+      style="margin-left: 5px; color: var(--mdc-theme-on-surface);"
+    >
+    </mwc-icon-button-toggle>
+  </div>
+</div>
+`;
+/* end snapshot foundation getCustomField renders a INT32U field correctly */
+
+snapshots["foundation getCustomField renders a FLOAT32 field correctly"] = 
+`<div style="display: flex; flex-direction: row;">
+  <div style="flex: auto;">
+    <label class="mdc-text-field mdc-text-field--disabled mdc-text-field--filled mdc-text-field--no-label">
+      <span class="mdc-text-field__ripple">
+      </span>
+      <input
+        class="mdc-text-field__input"
+        disabled=""
+        max="4294967295"
+        min="0"
+        placeholder=""
+        type="number"
+      >
+      <span class="mdc-line-ripple">
+      </span>
+    </label>
+  </div>
+  <div style="display: flex; align-items: center; height: 56px;">
+    <mwc-icon-button
+      disabled=""
+      icon="done"
+      id="doneButton"
+      style="color: var(--mdc-theme-on-surface); display: none;"
+    >
+    </mwc-icon-button>
+    <mwc-icon-button-toggle
+      officon="edit"
+      onicon="clear"
+      style="margin-left: 5px; color: var(--mdc-theme-on-surface);"
+    >
+    </mwc-icon-button-toggle>
+  </div>
+</div>
+`;
+/* end snapshot foundation getCustomField renders a FLOAT32 field correctly */
+
+snapshots["foundation getCustomField renders a FLOAT64 field correctly"] = 
+`<div style="display: flex; flex-direction: row;">
+  <div style="flex: auto;">
+    <label class="mdc-text-field mdc-text-field--disabled mdc-text-field--filled mdc-text-field--no-label">
+      <span class="mdc-text-field__ripple">
+      </span>
+      <input
+        class="mdc-text-field__input"
+        disabled=""
+        max="18446744073709552000"
+        min="-18446744073709552000"
+        placeholder=""
+        step="0.1"
+        type="number"
+      >
+      <span class="mdc-line-ripple">
+      </span>
+    </label>
+  </div>
+  <div style="display: flex; align-items: center; height: 56px;">
+    <mwc-icon-button
+      icon="done"
+      id="doneButton"
+      style="color: var(--mdc-theme-on-surface); display: none;"
+    >
+    </mwc-icon-button>
+    <mwc-icon-button-toggle
+      officon="edit"
+      onicon="clear"
+      style="margin-left: 5px; color: var(--mdc-theme-on-surface);"
+    >
+    </mwc-icon-button-toggle>
+  </div>
+</div>
+`;
+/* end snapshot foundation getCustomField renders a FLOAT64 field correctly */
+
+snapshots["foundation getCustomField renders a VisString32 field correctly"] = 
+`<div style="display: flex; flex-direction: row;">
+  <div style="flex: auto;">
+    <label class="mdc-text-field mdc-text-field--disabled mdc-text-field--filled mdc-text-field--no-label">
+      <span class="mdc-text-field__ripple">
+      </span>
+      <input
+        class="mdc-text-field__input"
+        disabled=""
+        maxlength="32"
+        placeholder=""
+        type="text"
+      >
+      <span class="mdc-line-ripple">
+      </span>
+    </label>
+  </div>
+  <div style="display: flex; align-items: center; height: 56px;">
+    <mwc-icon-button
+      icon="done"
+      id="doneButton"
+      style="color: var(--mdc-theme-on-surface); display: none;"
+    >
+    </mwc-icon-button>
+    <mwc-icon-button-toggle
+      officon="edit"
+      onicon="clear"
+      style="margin-left: 5px; color: var(--mdc-theme-on-surface);"
+    >
+    </mwc-icon-button-toggle>
+  </div>
+</div>
+`;
+/* end snapshot foundation getCustomField renders a VisString32 field correctly */
+
+snapshots["foundation getCustomField renders a VisString64 field correctly"] = 
+`<div style="display: flex; flex-direction: row;">
+  <div style="flex: auto;">
+    <label class="mdc-text-field mdc-text-field--disabled mdc-text-field--filled mdc-text-field--no-label">
+      <span class="mdc-text-field__ripple">
+      </span>
+      <input
+        class="mdc-text-field__input"
+        disabled=""
+        maxlength="64"
+        placeholder=""
+        type="text"
+      >
+      <span class="mdc-line-ripple">
+      </span>
+    </label>
+  </div>
+  <div style="display: flex; align-items: center; height: 56px;">
+    <mwc-icon-button
+      icon="done"
+      id="doneButton"
+      style="color: var(--mdc-theme-on-surface); display: none;"
+    >
+    </mwc-icon-button>
+    <mwc-icon-button-toggle
+      officon="edit"
+      onicon="clear"
+      style="margin-left: 5px; color: var(--mdc-theme-on-surface);"
+    >
+    </mwc-icon-button-toggle>
+  </div>
+</div>
+`;
+/* end snapshot foundation getCustomField renders a VisString64 field correctly */
+
+snapshots["foundation getCustomField renders a VisString65 field correctly"] = 
+`<div style="display: flex; flex-direction: row;">
+  <div style="flex: auto;">
+    <label class="mdc-text-field mdc-text-field--disabled mdc-text-field--filled mdc-text-field--no-label">
+      <span class="mdc-text-field__ripple">
+      </span>
+      <input
+        class="mdc-text-field__input"
+        disabled=""
+        maxlength="65"
+        placeholder=""
+        type="text"
+      >
+      <span class="mdc-line-ripple">
+      </span>
+    </label>
+  </div>
+  <div style="display: flex; align-items: center; height: 56px;">
+    <mwc-icon-button
+      icon="done"
+      id="doneButton"
+      style="color: var(--mdc-theme-on-surface); display: none;"
+    >
+    </mwc-icon-button>
+    <mwc-icon-button-toggle
+      officon="edit"
+      onicon="clear"
+      style="margin-left: 5px; color: var(--mdc-theme-on-surface);"
+    >
+    </mwc-icon-button-toggle>
+  </div>
+</div>
+`;
+/* end snapshot foundation getCustomField renders a VisString65 field correctly */
+
+snapshots["foundation getCustomField renders a VisString129 field correctly"] = 
+`<div style="display: flex; flex-direction: row;">
+  <div style="flex: auto;">
+    <label class="mdc-text-field mdc-text-field--disabled mdc-text-field--filled mdc-text-field--no-label">
+      <span class="mdc-text-field__ripple">
+      </span>
+      <input
+        class="mdc-text-field__input"
+        disabled=""
+        maxlength="129"
+        placeholder=""
+        type="text"
+      >
+      <span class="mdc-line-ripple">
+      </span>
+    </label>
+  </div>
+  <div style="display: flex; align-items: center; height: 56px;">
+    <mwc-icon-button
+      icon="done"
+      id="doneButton"
+      style="color: var(--mdc-theme-on-surface); display: none;"
+    >
+    </mwc-icon-button>
+    <mwc-icon-button-toggle
+      officon="edit"
+      onicon="clear"
+      style="margin-left: 5px; color: var(--mdc-theme-on-surface);"
+    >
+    </mwc-icon-button-toggle>
+  </div>
+</div>
+`;
+/* end snapshot foundation getCustomField renders a VisString129 field correctly */
+
+snapshots["foundation getCustomField renders a VisString255 field correctly"] = 
+`<div style="display: flex; flex-direction: row;">
+  <div style="flex: auto;">
+    <label class="mdc-text-field mdc-text-field--disabled mdc-text-field--filled mdc-text-field--no-label">
+      <span class="mdc-text-field__ripple">
+      </span>
+      <input
+        class="mdc-text-field__input"
+        disabled=""
+        maxlength="255"
+        placeholder=""
+        type="text"
+      >
+      <span class="mdc-line-ripple">
+      </span>
+    </label>
+  </div>
+  <div style="display: flex; align-items: center; height: 56px;">
+    <mwc-icon-button
+      icon="done"
+      id="doneButton"
+      style="color: var(--mdc-theme-on-surface); display: none;"
+    >
+    </mwc-icon-button>
+    <mwc-icon-button-toggle
+      officon="edit"
+      onicon="clear"
+      style="margin-left: 5px; color: var(--mdc-theme-on-surface);"
+    >
+    </mwc-icon-button-toggle>
+  </div>
+</div>
+`;
+/* end snapshot foundation getCustomField renders a VisString255 field correctly */
+
