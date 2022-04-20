6f3c5e0a44938c8f473aaf3e3ea05d7ef68b10e3
Dennis Labordus <dennis.labordus@alliander.com>
2022-04-20 13:15:46 +0200

    feat (iededitor/infowizards): Add Info Wizards for DO/DA in IED Editor.

--- src/editors/ied/server-container.ts
+++ src/editors/ied/server-container.ts
@@ -6,11 +6,14 @@ import {
   property,
   TemplateResult,
 } from 'lit-element';
+import { nothing } from "lit-html";
 
 import '../../action-pane.js';
+import './ldevice-container.js';
+
 import { Nsdoc } from '../../foundation/nsdoc.js';
 import { serverIcon } from '../../icons/ied-icons.js';
-import './ldevice-container.js';
+import { getDescriptionAttribute } from "../../foundation.js";
 
 /** [[`IED`]] plugin subeditor for editing `Server` element. */
 @customElement('server-container')
@@ -24,14 +27,16 @@ export class ServerContainer extends LitElement {
   @property()
   nsdoc!: Nsdoc;
 
-  private header(): string {
-    return 'Server';
+  private header(): TemplateResult {
+    const desc = getDescriptionAttribute(this.element);
+
+    return html`Server${desc ? html` &mdash; ${desc}` : nothing}`;
   }
 
   render(): TemplateResult {
-    return html`<action-pane label="${this.header()}">
+    return html`<action-pane .label="${this.header()}">
       <mwc-icon slot="icon">${serverIcon}</mwc-icon>
-      ${Array.from(this.element.querySelectorAll(':scope > LDevice')).map(server => 
+      ${Array.from(this.element.querySelectorAll(':scope > LDevice')).map(server =>
         html`<ldevice-container
           .element=${server}
           .nsdoc=${this.nsdoc}
