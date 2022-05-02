fa05ef7a72da57f6180a41b507a5433851965e8f
Flurb <private@tjalma.com>
2022-05-02 11:19:59 +0200

    Added unit tests

--- test/unit/editors/subscription/subscriber-ied-list.test.ts
+++ test/unit/editors/subscription/subscriber-ied-list.test.ts
@@ -1,4 +1,6 @@
 import { html, fixture, expect } from '@open-wc/testing';
+import { SinonSpy, spy } from 'sinon';
+import { newGOOSESelectEvent, newIEDSelectEvent, newViewEvent, View } from '../../../../src/editors/subscription/foundation.js';
 
 import '../../../../src/editors/subscription/subscriber-list.js'
 import { SubscriberList } from '../../../../src/editors/subscription/subscriber-list.js';
@@ -6,11 +8,16 @@ import { SubscriberList } from '../../../../src/editors/subscription/subscriber-
 describe('subscriber-list', () => {
   let element: SubscriberList;
   let validSCL: XMLDocument;
+  
+  let wizardEvent: SinonSpy;
 
   beforeEach(async () => {
     validSCL = await fetch('/test/testfiles/valid2007B4.scd')
       .then(response => response.text())
       .then(str => new DOMParser().parseFromString(str, 'application/xml'));
+
+    wizardEvent = spy();
+    window.addEventListener('wizard', wizardEvent);
 
     element = await fixture(html`<subscriber-list
       .doc=${validSCL}
@@ -19,5 +26,45 @@ describe('subscriber-list', () => {
 
   it('initially looks like the latest snapshot', async () => {
     await expect(element).shadowDom.to.equalSnapshot();
+  });
+
+  it('triggers edit wizard when an IED is selected', async () => {
+    const gseControl = validSCL.querySelector('IED[name="IED1"] > AccessPoint[name="P1"] > Server > LDevice > LN0 > GSEControl[appID="0001"]');
+    const dataSet = validSCL.querySelector('IED[name="IED1"] > AccessPoint[name="P1"] > Server > LDevice > LN0 > DataSet[name="GooseDataSet1"]');
+
+    element['onGOOSEDataSetEvent'](newGOOSESelectEvent(
+      gseControl!,
+      dataSet!
+    ));
+
+    await element.requestUpdate();
+
+    (<HTMLElement>(
+      element.shadowRoot?.querySelector('mwc-icon-button[icon="edit"]')
+    )).click();
+
+    expect(wizardEvent).to.have.be.calledOnce;
+    expect(wizardEvent.args[0][0].detail.wizard()[0].title).to.contain('edit');
+    expect(wizardEvent.args[0][0].detail.wizard()[0].element).to.eql(gseControl);
+  });
+
+  it('triggers edit wizard when a GOOSE is selected', async () => {
+    element['onViewChange'](newViewEvent(View.GOOSE_SUBSCRIBER));
+
+    const ied = validSCL.querySelector('IED[name="IED1"] > AccessPoint[name="P1"] > Server > LDevice > LN0 > GSEControl[appID="0001"]');
+
+    element['onIEDSelectEvent'](newIEDSelectEvent(
+      ied!,
+    ));
+
+    await element.requestUpdate();
+
+    (<HTMLElement>(
+      element.shadowRoot?.querySelector('mwc-icon-button[icon="edit"]')
+    )).click();
+
+    expect(wizardEvent).to.have.be.calledOnce;
+    expect(wizardEvent.args[0][0].detail.wizard()[0].title).to.contain('edit');
+    expect(wizardEvent.args[0][0].detail.wizard()[0].element).to.eql(ied);
   });
 });
