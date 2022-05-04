bae7366ddab08230a363435c0cecab9e74e6e757
Jakob Vogelsang <jakob-vogelsang@posteo.de>
2022-05-04 14:05:31 +0200

    feat(editors/substation/eqfunction): add action-pane based editor

---
+++ src/editors/substation/eqfunction-editor.ts
@@ -0,0 +1,35 @@
+import {
+  html,
+  LitElement,
+  TemplateResult,
+  property,
+  customElement,
+  state,
+} from 'lit-element';
+
+import '../../action-pane.js';
+
+/** Pane rendering `Function` element with its children */
+@customElement('eqfunction-editor')
+export class EqFunctionEditor extends LitElement {
+  /** The edited `EqFunction` element */
+  @property({ attribute: false })
+  element!: Element;
+  @state()
+  get header(): string {
+    const name = this.element.getAttribute('name');
+    const desc = this.element.getAttribute('desc');
+    const type = this.element.getAttribute('type');
+
+    return `${name}${desc ? ` - ${desc}` : ''}${type ? ` (${type})` : ''}`;
+  }
+
+  render(): TemplateResult {
+    return html`<action-pane
+      label="${this.header}"
+      icon="functions"
+      secondary
+      highlighted
+    ></action-pane>`;
+  }
+}
---
+++ test/unit/editors/substation/eqfunction-editor.test.ts
@@ -0,0 +1,45 @@
+import { fixture, html, expect } from '@open-wc/testing';
+
+import '../../../../src/editors/substation/eqfunction-editor.js';
+import { EqFunctionEditor } from '../../../../src/editors/substation/eqfunction-editor.js';
+
+describe('web component rendering EqFunction element', () => {
+  let element: EqFunctionEditor;
+  let doc: XMLDocument;
+
+  beforeEach(async () => {
+    doc = await fetch('/test/testfiles/zeroline/functions.scd')
+      .then(response => response.text())
+      .then(str => new DOMParser().parseFromString(str, 'application/xml'));
+  });
+
+  describe('with complete attribute set and existing children', () => {
+    beforeEach(async () => {
+      element = <EqFunctionEditor>(
+        await fixture(
+          html`<eqfunction-editor
+            .element=${doc.querySelector('ConductingEquipment EqFunction')}
+          ></eqfunction-editor>`
+        )
+      );
+    });
+
+    it('looks like the latest snapshot', async () =>
+      await expect(element).shadowDom.to.equalSnapshot());
+  });
+
+  describe('with missing desc and type attribute', () => {
+    beforeEach(async () => {
+      element = <EqFunctionEditor>(
+        await fixture(
+          html`<eqfunction-editor
+            .element=${doc.querySelector('EqFunction')}
+          ></eqfunction-editor>`
+        )
+      );
+    });
+
+    it('looks like the latest snapshot', async () =>
+      await expect(element).shadowDom.to.equalSnapshot());
+  });
+});
