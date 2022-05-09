d15783013c8065efa73ac68f73ff8638dc5db480
Jakob Vogelsang <jakob-vogelsang@posteo.de>
2022-05-09 15:32:40 +0200

    feat(editors/subtation): add l-node-editor to Function

--- src/editors/substation/function-editor.ts
+++ src/editors/substation/function-editor.ts
@@ -26,6 +26,18 @@ export class FunctionEditor extends LitElement {
     return `${name}${desc ? ` - ${desc}` : ''}${type ? ` (${type})` : ''}`;
   }
 
+  private renderLNodes(): TemplateResult {
+    const lNodes = getChildElementsByTagName(this.element, 'LNode');
+
+    return lNodes.length
+      ? html`<div class="container lnode">
+          ${lNodes.map(
+            lNode => html`<l-node-editor .element=${lNode}></l-node-editor>`
+          )}
+        </div>`
+      : html``;
+  }
+
   private renderSubFunctions(): TemplateResult {
     const subfunctions = getChildElementsByTagName(this.element, 'SubFunction');
     return html` ${subfunctions.map(
@@ -42,7 +54,7 @@ export class FunctionEditor extends LitElement {
       icon="functions"
       secondary
       highlighted
-      >${this.renderSubFunctions()}</action-pane
+      >${this.renderLNodes()}${this.renderSubFunctions()}</action-pane
     >`;
   }
 }
