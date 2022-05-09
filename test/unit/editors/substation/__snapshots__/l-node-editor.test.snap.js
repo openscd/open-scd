365dea0bdc642520a85327c6978dfbac0b6145cc
Jakob Vogelsang <jakob-vogelsang@posteo.de>
2022-05-09 14:54:03 +0200

    feat(editors/substation): add read-only l-node-editor

---
+++ test/unit/editors/substation/__snapshots__/l-node-editor.test.snap.js
@@ -0,0 +1,27 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["web component rendering LNode element as reference to a LN/LN0 within IED  looks like the latest snapshot"] = 
+`<action-icon
+  label="IED1 CircuitBreaker_CB1/ XCBR 1"
+  tabindex="0"
+>
+  <mwc-icon slot="icon">
+  </mwc-icon>
+</action-icon>
+`;
+/* end snapshot web component rendering LNode element as reference to a LN/LN0 within IED  looks like the latest snapshot */
+
+snapshots["web component rendering LNode element as instance of a LNodeType only looks like the latest snapshot"] = 
+`<action-icon
+  highlighted=""
+  label="DC XSWI 1"
+  secondary=""
+  tabindex="0"
+>
+  <mwc-icon slot="icon">
+  </mwc-icon>
+</action-icon>
+`;
+/* end snapshot web component rendering LNode element as instance of a LNodeType only looks like the latest snapshot */
+
