d3c041955d4e191ef3b876da034226ea1926e8ea
Jakob Vogelsang <jakob-vogelsang@posteo.de>
2022-05-23 20:20:44 +0200

    feat(wizards/subfunction): add edit wizard (#763)

--- test/unit/wizards/__snapshots__/subfunction.test.snap.js
+++ test/unit/wizards/__snapshots__/subfunction.test.snap.js
@@ -50,3 +50,50 @@ snapshots["Wizards for SCL Function element define an create wizard that looks l
 `;
 /* end snapshot Wizards for SCL Function element define an create wizard that looks like the the latest snapshot */
 
+snapshots["Wizards for SCL Function element define an edit wizard that looks like the the latest snapshot"] = 
+`<mwc-dialog
+  defaultaction="close"
+  heading="[wizard.title.edit]"
+  open=""
+  style="--mdc-dialog-min-width:calc(100% + 0px)"
+>
+  <div id="wizard-content">
+    <wizard-textfield
+      dialoginitialfocus=""
+      helper="[scl.name]"
+      label="name"
+      required=""
+      validationmessage="[textfield.required]"
+    >
+    </wizard-textfield>
+    <wizard-textfield
+      helper="[scl.desc]"
+      label="desc"
+      nullable=""
+    >
+    </wizard-textfield>
+    <wizard-textfield
+      helper="[scl.type]"
+      label="type"
+      nullable=""
+    >
+    </wizard-textfield>
+  </div>
+  <mwc-button
+    dialogaction="close"
+    label="[cancel]"
+    slot="secondaryAction"
+    style="--mdc-theme-primary: var(--mdc-theme-error)"
+  >
+  </mwc-button>
+  <mwc-button
+    icon="save"
+    label="[save]"
+    slot="primaryAction"
+    trailingicon=""
+  >
+  </mwc-button>
+</mwc-dialog>
+`;
+/* end snapshot Wizards for SCL Function element define an edit wizard that looks like the the latest snapshot */
+
