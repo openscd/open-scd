63b6bb9a335d506b3b64b0af7ff06a9e74d4c04e
Flurb <private@tjalma.com>
2022-05-24 22:48:39 +0200

    Added unit tests network-container

---
+++ test/unit/editors/protocol104/__snapshots__/network-container.test.snap.js
@@ -0,0 +1,19 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["network-104-container looks like the latest snapshot"] = 
+`<mwc-fab
+  extended=""
+  icon="add"
+  label="[subnetwork.wizard.title.add]"
+>
+</mwc-fab>
+<section>
+  <subnetwork-104-editor>
+  </subnetwork-104-editor>
+  <subnetwork-104-editor>
+  </subnetwork-104-editor>
+</section>
+`;
+/* end snapshot network-104-container looks like the latest snapshot */
+
