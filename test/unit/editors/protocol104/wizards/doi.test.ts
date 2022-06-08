53a3ccd287d2fb5a6ce611bef416d44bc4c557d5
Dennis Labordus <dennis.labordus@alliander.com>
2022-06-08 12:08:33 +0200

    Fixed Wizard Menu.
    Signed-off-by: Dennis Labordus <dennis.labordus@alliander.com>

--- test/unit/editors/protocol104/wizards/doi.test.ts
+++ test/unit/editors/protocol104/wizards/doi.test.ts
@@ -1,20 +1,17 @@
 import { expect, fixture, html } from '@open-wc/testing';
 
-import { MockWizard } from "../../../../mock-wizard.js";
+import { MockWizard } from '../../../../mock-wizard.js';
 
 import '../../../../mock-wizard.js';
 
-import { ComplexAction, isSimple, WizardInputElement } from "../../../../../src/foundation.js";
+import { showDOIInfoWizard } from '../../../../../src/editors/protocol104/wizards/doi.js';
 
-import { remove104Private, showDOIInfoWizard } from "../../../../../src/editors/protocol104/wizards/doi.js";
-
-import { fetchDoc } from "../../../wizards/test-support.js";
+import { fetchDoc } from '../../../wizards/test-support.js';
 
 describe('Wizards for 104 DOI Element', () => {
   let doc: XMLDocument;
   let doi: Element;
   let element: MockWizard;
-  let inputs: WizardInputElement[];
 
   beforeEach(async () => {
     doc = await fetchDoc('/test/testfiles/104/valid-addresses.scd');
@@ -23,12 +20,13 @@ describe('Wizards for 104 DOI Element', () => {
 
   describe('show 104 DOI Info', () => {
     beforeEach(async () => {
-      doi = doc.querySelector('IED[name="B1"] LN[lnType="SE_GGIO_SET_V002"] DOI[name="CntVal1"]')!;
+      doi = doc.querySelector(
+        'IED[name="B1"] LN[lnType="SE_GGIO_SET_V002"] DOI[name="CntVal1"]'
+      )!;
 
       const wizard = showDOIInfoWizard(doi);
       element.workflow.push(() => wizard);
       await element.requestUpdate();
-      inputs = Array.from(element.wizardUI.inputs);
     });
 
     it('looks like the latest snapshot', async () => {
@@ -38,36 +36,17 @@ describe('Wizards for 104 DOI Element', () => {
 
   describe('show 104 DOI Info with ctlModel', () => {
     beforeEach(async () => {
-      doi = doc.querySelector('IED[name="B1"] LN[lnType="SE_GGIO_SET_V002"] DOI[name="CmdBlk"]')!;
+      doi = doc.querySelector(
+        'IED[name="B1"] LN[lnType="SE_GGIO_SET_V002"] DOI[name="CmdBlk"]'
+      )!;
 
       const wizard = showDOIInfoWizard(doi);
       element.workflow.push(() => wizard);
       await element.requestUpdate();
-      inputs = Array.from(element.wizardUI.inputs);
     });
 
     it('looks like the latest snapshot', async () => {
       await expect(element.wizardUI.dialog).dom.to.equalSnapshot();
     });
-  });
-
-  describe('remove 104 Private elements', () => {
-    it('return expected number of Delete Actions when there are 104 Private elements', () => {
-      doi = doc.querySelector('IED[name="B1"] LN[lnType="SE_GGIO_SET_V002"] DOI[name="Mod"]')!;
-      const actions = remove104Private(doi)();
-
-      expect(actions.length).to.equal(1);
-      expect(actions[0]).to.not.satisfy(isSimple);
-
-      const complexAction = <ComplexAction>actions[0];
-      expect(complexAction.actions.length).to.equal(2);
-    })
-
-    it('return no Delete Actions when there are no 104 Private elements', () => {
-      doi = doc.querySelector('IED[name="B1"] LN[lnType="SE_GSAL_SET_V001"] DOI[name="OpCntRs"]')!;
-      const actions = remove104Private(doi)();
-
-      expect(actions.length).to.equal(0);
-    })
   });
 });
