4d52a9a3e3aee64ee695606b046b85afdbddb2a8
Dennis Labordus <dennis.labordus@alliander.com>
2022-08-30 09:52:04 +0200

    feat(wizard/dai): Set/Update a value for a type 'Timestamp' of Data Attribute Instance (#959)
    * Added functionality to add/edit Timestamp values by the Data Attributes.
    * Added some extra test for DAI Wizard.
    * Review comment processed.

--- src/wizards/dai.ts
+++ src/wizards/dai.ts
@@ -1,53 +1,65 @@
 import { html, TemplateResult } from 'lit-element';
-import { nothing } from "lit-html";
+import { nothing } from 'lit-html';
 import { get } from 'lit-translate';
-import { DaiValidationTypes, getCustomField } from '../editors/ied/foundation/foundation.js';
+
+import { DaiFieldTypes, getCustomField } from './foundation/dai-field-type.js';
 
 import '../../src/wizard-textfield.js';
 
 import {
   ComplexAction,
   EditorAction,
-  getValue,
   Wizard,
   WizardActor,
   WizardInputElement,
 } from '../foundation.js';
-import { SCL_NAMESPACE } from "../schemas.js";
+import { SCL_NAMESPACE } from '../schemas.js';
 
-export function updateValue(instanceElement: Element): WizardActor {
+export function updateValue(
+  element: Element,
+  instanceElement: Element
+): WizardActor {
   return (inputs: WizardInputElement[]): EditorAction[] => {
-    const newValue = getValue(inputs.find(i => i.value)!)!;
-    const name = instanceElement.getAttribute('name');
+    const bType = element.getAttribute('bType')!;
+    const newValue = getCustomField()[<DaiFieldTypes>bType].value(inputs);
 
+    const name = instanceElement.getAttribute('name');
     const complexAction: ComplexAction = {
       actions: [],
-      title: get('dai.action.updatedai', {daiName: name!}),
+      title: get('dai.action.updatedai', { daiName: name! }),
     };
 
     const oldVal = instanceElement.querySelector('Val');
     if (oldVal) {
       const newVal = <Element>oldVal.cloneNode(false);
       newVal.textContent = newValue;
-      complexAction.actions.push({old: {element: oldVal}, new: {element: newVal}});
+      complexAction.actions.push({
+        old: { element: oldVal },
+        new: { element: newVal },
+      });
     } else {
-      const newVal = instanceElement.ownerDocument.createElementNS(SCL_NAMESPACE, 'Val');
+      const newVal = instanceElement.ownerDocument.createElementNS(
+        SCL_NAMESPACE,
+        'Val'
+      );
       newVal.textContent = newValue;
-      complexAction.actions.push({new: {parent: instanceElement, element: newVal}});
+      complexAction.actions.push({
+        new: { parent: instanceElement, element: newVal },
+      });
     }
     return [complexAction];
   };
 }
 
-export function createValue(parent: Element, newElement: Element, instanceElement: Element): WizardActor {
+export function createValue(
+  parent: Element,
+  element: Element,
+  newElement: Element,
+  instanceElement: Element
+): WizardActor {
   return (inputs: WizardInputElement[]): EditorAction[] => {
-    const newValue = getValue(inputs.find(i => i.value)!)!;
-    const name = instanceElement.getAttribute('name');
-
-    const complexAction: ComplexAction = {
-      actions: [],
-      title: get('dai.action.createdai', {daiName: name!}),
-    };
+    const bType = element.getAttribute('bType')!;
+    const newValue = getCustomField()[<DaiFieldTypes>bType].value(inputs);
 
     let valElement = instanceElement.querySelector('Val');
     if (!valElement) {
@@ -55,7 +67,12 @@ export function createValue(parent: Element, newElement: Element, instanceElemen
       instanceElement.append(valElement);
     }
     valElement.textContent = newValue;
-    complexAction.actions.push({new: {parent, element: newElement}});
+
+    const name = instanceElement.getAttribute('name');
+    const complexAction: ComplexAction = {
+      actions: [{ new: { parent, element: newElement } }],
+      title: get('dai.action.createdai', { daiName: name! }),
+    };
     return [complexAction];
   };
 }
@@ -68,49 +85,64 @@ export function renderDAIWizard(
   const daValue = element.querySelector('Val')?.textContent?.trim() ?? '';
 
   return [
-    html`
-      ${getCustomField()[<DaiValidationTypes>bType].render(element, instanceElement)}
-      ${daValue
-        ? html`<wizard-textfield label="DA Template Value"
-                 .maybeValue=${daValue}
-                 readonly
-                 disabled>
-               </wizard-textfield>`
-        : nothing}`,
+    html` ${getCustomField()[<DaiFieldTypes>bType].render(
+      element,
+      instanceElement
+    )}
+    ${daValue
+      ? html`<wizard-textfield
+          id="daVal"
+          label="DA Template Value"
+          .maybeValue=${daValue}
+          readonly
+          disabled
+        >
+        </wizard-textfield>`
+      : nothing}`,
   ];
 }
 
-export function createDAIWizard(parent: Element, newElement: Element, element: Element): Wizard {
+export function createDAIWizard(
+  parent: Element,
+  newElement: Element,
+  element: Element
+): Wizard {
   // Retrieve the created DAI, can be the new element or one of the child elements below.
-  const instanceElement = (newElement.tagName === 'DAI') ? newElement : newElement.querySelector('DAI')!;
+  const instanceElement =
+    newElement.tagName === 'DAI'
+      ? newElement
+      : newElement.querySelector('DAI')!;
 
   return [
     {
       title: get('dai.wizard.title.create', {
-        daiName: instanceElement?.getAttribute('name') ?? ''
+        daiName: instanceElement?.getAttribute('name') ?? '',
       }),
       element: instanceElement,
       primary: {
         icon: 'edit',
         label: get('save'),
-        action: createValue(parent, newElement, instanceElement),
+        action: createValue(parent, element, newElement, instanceElement),
       },
       content: renderDAIWizard(element, instanceElement),
     },
   ];
 }
 
-export function editDAIWizard(element: Element, instanceElement?: Element): Wizard {
+export function editDAIWizard(
+  element: Element,
+  instanceElement?: Element
+): Wizard {
   return [
     {
       title: get('dai.wizard.title.edit', {
-        daiName: instanceElement?.getAttribute('name') ?? ''
+        daiName: instanceElement?.getAttribute('name') ?? '',
       }),
       element: instanceElement,
       primary: {
         icon: 'edit',
         label: get('save'),
-        action: updateValue(instanceElement!),
+        action: updateValue(element, instanceElement!),
       },
       content: renderDAIWizard(element, instanceElement),
     },
