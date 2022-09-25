faa555403e19ecabdd92a8096723536a5b371bf6
Jakob Vogelsang <jakob-vogelsang@posteo.de>
2022-09-25 22:24:32 +0200

    feat(wizards/connectedap): create GSE and SMV

--- src/wizards/connectedap.ts
+++ src/wizards/connectedap.ts
@@ -24,6 +24,7 @@ import {
   ComplexAction,
   isPublic,
   identity,
+  SimpleAction,
 } from '../foundation.js';
 import {
   getTypes,
@@ -31,6 +32,10 @@ import {
   typeNullable,
   typePattern,
 } from './foundation/p-types.js';
+import {
+  mACAddressGenerator,
+  appIdGenerator,
+} from '../foundation/generators.js';
 
 interface AccessPointDescription {
   element: Element;
@@ -46,28 +51,243 @@ function compareAccessPointConnection(
   return 0;
 }
 
+function initSMVElements(
+  doc: XMLDocument,
+  connectedAp: Element,
+  options: {
+    macGeneratorSmv: () => string;
+    appidGeneratorSmv: () => string;
+    unconnectedSampledValueControl: Set<string>;
+  }
+): SimpleAction[] {
+  const actions: SimpleAction[] = [];
+
+  const ied = doc.querySelector(
+    `IED[name="${connectedAp.getAttribute('iedName')}"]`
+  );
+
+  Array.from(ied?.querySelectorAll('SampledValueControl') ?? [])
+    .filter(sampledValueControl => {
+      const id = identity(sampledValueControl) as string;
+
+      if (options.unconnectedSampledValueControl.has(id)) {
+        options.unconnectedSampledValueControl.delete(id);
+        return true;
+      }
+
+      return false;
+    })
+    .forEach(sampledValueControl => {
+      const cbName = sampledValueControl.getAttribute('name');
+      const ldInst =
+        sampledValueControl.closest('LDevice')?.getAttribute('inst') ?? null;
+
+      const sMV = createElement(connectedAp.ownerDocument, 'SMV', {
+        cbName,
+        ldInst,
+      });
+      actions.push({ new: { parent: connectedAp, element: sMV } });
+
+      const address = createElement(connectedAp.ownerDocument, 'Address', {});
+      actions.push({ new: { parent: sMV, element: address } });
+
+      const pMac = createElement(connectedAp.ownerDocument, 'P', {
+        type: 'MAC-Address',
+      });
+      pMac.textContent = options.macGeneratorSmv();
+      actions.push({ new: { parent: address, element: pMac } });
+
+      const pAppId = createElement(connectedAp.ownerDocument, 'P', {
+        type: 'APPID',
+      });
+      pAppId.textContent = options.appidGeneratorSmv();
+      actions.push({ new: { parent: address, element: pAppId } });
+
+      const pVlanId = createElement(connectedAp.ownerDocument, 'P', {
+        type: 'VLANID',
+      });
+      pVlanId.textContent = '000';
+      actions.push({ new: { parent: address, element: pVlanId } });
+
+      const pVlanPrio = createElement(connectedAp.ownerDocument, 'P', {
+        type: 'VLAN-Priority',
+      });
+      pVlanPrio.textContent = '4';
+      actions.push({ new: { parent: address, element: pVlanPrio } });
+    });
+
+  return actions;
+}
+
+function initGSEElements(
+  doc: XMLDocument,
+  connectedAp: Element,
+  options: {
+    macGeneratorGse: () => string;
+    appidGeneratorGse: () => string;
+    unconnectedGseControl: Set<string>;
+  }
+): SimpleAction[] {
+  const actions: SimpleAction[] = [];
+
+  const ied = doc.querySelector(
+    `IED[name="${connectedAp.getAttribute('iedName')}"]`
+  );
+
+  Array.from(ied?.querySelectorAll('GSEControl') ?? [])
+    .filter(gseControl => {
+      const id = identity(gseControl) as string;
+
+      if (options.unconnectedGseControl.has(id)) {
+        options.unconnectedGseControl.delete(id);
+        return true;
+      }
+
+      return false;
+    })
+    .forEach(gseControl => {
+      const cbName = gseControl.getAttribute('name');
+      const ldInst =
+        gseControl.closest('LDevice')?.getAttribute('inst') ?? null;
+
+      const gSE = createElement(connectedAp.ownerDocument, 'GSE', {
+        cbName,
+        ldInst,
+      });
+      actions.push({ new: { parent: connectedAp, element: gSE } });
+
+      const address = createElement(connectedAp.ownerDocument, 'Address', {});
+      actions.push({ new: { parent: gSE, element: address } });
+
+      const pMac = createElement(connectedAp.ownerDocument, 'P', {
+        type: 'MAC-Address',
+      });
+      pMac.textContent = options.macGeneratorGse();
+      actions.push({ new: { parent: address, element: pMac } });
+
+      const pAppId = createElement(connectedAp.ownerDocument, 'P', {
+        type: 'APPID',
+      });
+      pAppId.textContent = options.appidGeneratorGse();
+      actions.push({ new: { parent: address, element: pAppId } });
+
+      const pVlanId = createElement(connectedAp.ownerDocument, 'P', {
+        type: 'VLANID',
+      });
+      pVlanId.textContent = '000';
+      actions.push({ new: { parent: address, element: pVlanId } });
+
+      const pVlanPrio = createElement(connectedAp.ownerDocument, 'P', {
+        type: 'VLAN-Priority',
+      });
+      pVlanPrio.textContent = '4';
+      actions.push({ new: { parent: address, element: pVlanPrio } });
+
+      const minTime = createElement(connectedAp.ownerDocument, 'MinTime', {
+        unit: 's',
+        multiplier: 'm',
+      });
+      minTime.textContent = '10';
+      actions.push({ new: { parent: gSE, element: minTime } });
+
+      const maxTime = createElement(connectedAp.ownerDocument, 'MaxTime', {
+        unit: 's',
+        multiplier: 'm',
+      });
+      maxTime.textContent = '10000';
+      actions.push({ new: { parent: gSE, element: maxTime } });
+    });
+
+  return actions;
+}
+
+function unconnectedGseControls(doc: XMLDocument): Set<string> {
+  const allGseControl = Array.from(doc.querySelectorAll('GSEControl'));
+
+  const unconnectedGseControl = allGseControl
+    .filter(gseControl => {
+      const iedName = gseControl.closest('IED')?.getAttribute('name');
+      const ldInst = gseControl.closest('LDevice')?.getAttribute('inst');
+      const cbName = gseControl.getAttribute('name');
+
+      return !doc.querySelector(
+        `ConnectedAP[iedName="${iedName}"] ` +
+          `> GSE[ldInst="${ldInst}"][cbName="${cbName}"]`
+      );
+    })
+    .map(gseControl => identity(gseControl) as string);
+
+  const mySet = new Set(unconnectedGseControl);
+  return mySet;
+}
+
+function unconnectedSampledValueControls(doc: XMLDocument): Set<string> {
+  const allSmvControl = Array.from(doc.querySelectorAll('SampledValueControl'));
+
+  const unconnectedSmvControl = allSmvControl
+    .filter(gseControl => {
+      const iedName = gseControl.closest('IED')?.getAttribute('name');
+      const ldInst = gseControl.closest('LDevice')?.getAttribute('inst');
+      const cbName = gseControl.getAttribute('name');
+
+      return !doc.querySelector(
+        `ConnectedAP[iedName="${iedName}"] ` +
+          `> SMV[ldInst="${ldInst}"][cbName="${cbName}"]`
+      );
+    })
+    .map(gseControl => identity(gseControl) as string);
+
+  const mySet = new Set(unconnectedSmvControl);
+  return mySet;
+}
+
 function createConnectedApAction(parent: Element): WizardActor {
   return (
     _: WizardInputElement[],
     __: Element,
     list?: List | null
   ): EditorAction[] => {
+    const doc = parent.ownerDocument;
+
+    // generators ensure unique MAC-Address and APPID across the project
+    const macGeneratorSmv = mACAddressGenerator(doc, 'SMV');
+    const appidGeneratorSmv = appIdGenerator(doc, 'SMV');
+    const macGeneratorGse = mACAddressGenerator(doc, 'GSE');
+    const appidGeneratorGse = appIdGenerator(doc, 'GSE');
+
+    // track GSE and SMV for multiselect access points connection
+    const unconnectedGseControl = unconnectedGseControls(doc);
+    const unconnectedSampledValueControl = unconnectedSampledValueControls(doc);
+
     if (!list) return [];
 
     const identities = (<ListItemBase[]>list.selected).map(item => item.value);
 
     const actions = identities.map(identity => {
       const [iedName, apName] = identity.split('>');
+      const actions: SimpleAction[] = [];
 
-      return {
-        new: {
-          parent,
-          element: createElement(parent.ownerDocument, 'ConnectedAP', {
-            iedName,
-            apName,
-          }),
-        },
-      };
+      const connectedAp = createElement(parent.ownerDocument, 'ConnectedAP', {
+        iedName,
+        apName,
+      });
+      actions.push({ new: { parent, element: connectedAp } });
+      actions.push(
+        ...initSMVElements(doc, connectedAp, {
+          macGeneratorSmv,
+          appidGeneratorSmv,
+          unconnectedSampledValueControl,
+        })
+      );
+      actions.push(
+        ...initGSEElements(doc, connectedAp, {
+          macGeneratorGse,
+          appidGeneratorGse,
+          unconnectedGseControl,
+        })
+      );
+
+      return { title: 'Added ConnectedAP', actions };
     });
 
     return actions;
@@ -90,27 +310,31 @@ function existConnectedAp(accesspoint: Element): boolean {
  * @param element - The ConnectedAP of the wizard.
  * @returns The checkbox within a formfield.
  */
-export function createTypeRestrictionCheckbox(element: Element): TemplateResult {
+export function createTypeRestrictionCheckbox(
+  element: Element
+): TemplateResult {
   return html`<mwc-formfield
     label="${translate('connectedap.wizard.addschemainsttype')}"
     ><mwc-checkbox
       id="typeRestriction"
       ?checked=${hasTypeRestriction(element)}
     ></mwc-checkbox>
-  </mwc-formfield>`
+  </mwc-formfield>`;
 }
 
-export function createPTextField(element: Element, pType: string): TemplateResult {
+export function createPTextField(
+  element: Element,
+  pType: string
+): TemplateResult {
   return html`<wizard-textfield
     required
     label="${pType}"
     pattern="${ifDefined(typePattern[pType])}"
     ?nullable=${typeNullable[pType]}
-    .maybeValue=${element.querySelector(
-      `Address > P[type="${pType}"]`
-    )?.innerHTML ?? null}
+    .maybeValue=${element.querySelector(`Address > P[type="${pType}"]`)
+      ?.innerHTML ?? null}
     maxLength="${ifDefined(typeMaxLength[pType])}"
-  ></wizard-textfield>`
+  ></wizard-textfield>`;
 }
 
 /** @returns single page  [[`Wizard`]] for creating SCL element ConnectedAP. */
@@ -251,9 +475,9 @@ export function editConnectedApWizard(element: Element): Wizard {
       },
       content: [
         html`${createTypeRestrictionCheckbox(element)}
-          ${getTypes(element).map(
-            pType => html`${createPTextField(element, pType)}`
-          )}`,
+        ${getTypes(element).map(
+          pType => html`${createPTextField(element, pType)}`
+        )}`,
       ],
     },
   ];
