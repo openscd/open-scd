20085718a4aad7d4dfa76660eb4d25722a52ab03
Juan Muñoz <juan.munoz@alliander.com>
2022-10-17 18:27:40 +0200

    test: LGOS tests

--- test/unit/editors/subscription/supervision.test.ts
+++ test/unit/editors/subscription/supervision.test.ts
@@ -102,4 +102,94 @@ describe('supervision', () => {
       expect(lnElement.nodeName).to.be.equal('LN');
     });
   });
+  describe('when subscribing to a GSEControl', () => {
+    beforeEach(async () => {
+      doc = await fetch(
+        '/test/testfiles/editors/VS893_LaterBindingGOOSE-LGOS.scd'
+      )
+        .then(response => response.text())
+        .then(str => new DOMParser().parseFromString(str, 'application/xml'));
+    });
+
+    it('produces the correct structure where supervision is allowed', () => {
+      controlElement = doc.querySelector(
+        'IED[name="GOOSE_Publisher2"] GSEControl[name="GOOSE1"]'
+      )!;
+      subscriberIED = doc.querySelector('IED[name="GOOSE_Subscriber4"]')!;
+      const noSupervisionAllowedActions: Create[] =
+        instantiateSubscriptionSupervision(controlElement, subscriberIED);
+      expect(noSupervisionAllowedActions).to.have.length(0);
+
+      subscriberIED = doc.querySelector('IED[name="GOOSE_Subscriber2"]')!;
+      const createActions: Create[] = instantiateSubscriptionSupervision(
+        controlElement,
+        subscriberIED
+      );
+      expect(createActions).to.have.length(1);
+      const createAction: Create = createActions[0];
+      const parentElement: Node = createAction.new.parent;
+      const newElement: Node = createAction.new.element;
+      expect(parentElement).to.exist;
+      expect(newElement).to.exist;
+      expect(parentElement.nodeName).to.be.equal('LN');
+      expect(newElement.nodeName).to.be.equal('DOI');
+
+      controlElement = doc.querySelector(
+        'IED[name="GOOSE_Publisher"] GSEControl[name="GOOSE1"]'
+      )!;
+      subscriberIED = doc.querySelector('IED[name="GOOSE_Subscriber1"]')!;
+
+      const createActionsWithNewLn: Create[] =
+        instantiateSubscriptionSupervision(controlElement, subscriberIED);
+      expect(createActionsWithNewLn).to.have.length(2);
+      const newLnAction: Create = createActionsWithNewLn[0];
+      const lDeviceParent: Node = newLnAction.new.parent;
+      const newLnElement: Node = newLnAction.new.element;
+      expect(lDeviceParent).to.exist;
+      expect(newLnElement).to.exist;
+      expect(lDeviceParent.nodeName).to.be.equal('LDevice');
+      expect(newLnElement.nodeName).to.be.equal('LN');
+
+      const newDoiAction: Create = createActionsWithNewLn[1];
+      const lnParent: Node = newDoiAction.new.parent;
+      const newDoiElement: Node = newDoiAction.new.element;
+      expect(lnParent).to.exist;
+      expect(newDoiElement).to.exist;
+      expect(lnParent.nodeName).to.be.equal('LN');
+      expect(newDoiElement.nodeName).to.be.equal('DOI');
+    });
+
+    it('removes the correct elements when supervision is removed', () => {
+      controlElement = doc.querySelector(
+        'IED[name="GOOSE_Publisher"] GSEControl[name="GOOSE2"]'
+      )!;
+      subscriberIED = doc.querySelector('IED[name="GOOSE_Subscriber1"]')!;
+      const removeActions: Delete[] = removeSubscriptionSupervision(
+        controlElement,
+        subscriberIED
+      );
+      expect(removeActions).to.have.length(1);
+      const removeAction: Delete = removeActions[0];
+      const parentElement: Node = removeAction.old.parent;
+      const element: Node = removeAction.old.element;
+      expect(parentElement).to.exist;
+      expect(element).to.exist;
+      expect(parentElement.nodeName).to.be.equal('LN');
+      expect(element.nodeName).to.be.equal('DOI');
+
+      controlElement = doc.querySelector(
+        'IED[name="GOOSE_Publisher2"] GSEControl[name="GOOSE2"]'
+      )!;
+      const removeActionsWithRemovedLn: Delete[] =
+        removeSubscriptionSupervision(controlElement, subscriberIED);
+      expect(removeActionsWithRemovedLn).to.have.length(1);
+      const removeLnAction: Delete = removeActionsWithRemovedLn[0];
+      const lnParent: Node = removeLnAction.old.parent;
+      const lnElement: Node = removeLnAction.old.element;
+      expect(lnParent).to.exist;
+      expect(lnElement).to.exist;
+      expect(lnParent.nodeName).to.be.equal('LDevice');
+      expect(lnElement.nodeName).to.be.equal('LN');
+    });
+  });
 });
