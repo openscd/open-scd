b0660e8d9828bbd197270ccd79cc9b6c11153397
Jakob Vogelsang <jakob-vogelsang@posteo.de>
2022-10-17 13:54:52 +0200

    test(editors/substation/general-equipment): initial unit tests

---
+++ test/unit/editors/substation/__snapshots__/general-equipment-editor.test.snap.js
@@ -0,0 +1,20 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["Editor web component for GeneralEquipment SCL element rendered as action icon looks like the latest snapshot"] = 
+`<action-icon label="genSub —  someDesc">
+  <mwc-icon slot="icon">
+  </mwc-icon>
+</action-icon>
+`;
+/* end snapshot Editor web component for GeneralEquipment SCL element rendered as action icon looks like the latest snapshot */
+
+snapshots["Editor web component for GeneralEquipment SCL element rendered as action pane look like the latest snapshot"] = 
+`<action-pane
+  label="genSub —  someDesc"
+  tabindex="0"
+>
+</action-pane>
+`;
+/* end snapshot Editor web component for GeneralEquipment SCL element rendered as action pane look like the latest snapshot */
+
