7404a59828377f9a8032046d4bd1679353c2eb69
Christian Dinkel <christian.dinkel@omicronenergy.com>
2022-11-15 12:28:07 +0100

    Updates

---
+++ db55d4e3.js
@@ -0,0 +1,1 @@
+import"https://openscd.github.io/open-scd-core/open-scd.js";const o=document.querySelector("open-scd"),e=new URL(document.location).searchParams;for(const[t,c]of e)o.setAttribute(t,c);
