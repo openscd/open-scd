d23b4f6991ffc4597d944b15b54a8415d112eca9
Stef3st <steffen.van.den.driest@alliander.com>
2022-12-21 15:53:07 +0100

    Fixed snapshot test

--- test/unit/editors/ied/__snapshots__/access-point-container.test.snap.js
+++ test/unit/editors/ied/__snapshots__/access-point-container.test.snap.js
@@ -6,16 +6,32 @@ snapshots[
 ] = `<action-pane tabindex="0">
   <mwc-icon slot="icon">
   </mwc-icon>
+  <abbr
+    slot="action"
+    title="[edit]"
+  >
+    <mwc-icon-button icon="edit">
+    </mwc-icon-button>
+  </abbr>
   <server-container>
   </server-container>
   <div id="lnContainer">
   </div>
 </action-pane>
 `;
+/* end snapshot access-point-container with Server Elements looks like the latest snapshot */
+
 snapshots["access-point-container with LN Elements and all LN Classes displayed looks like the latest snapshot"] = 
 `<action-pane tabindex="0">
   <mwc-icon slot="icon">
   </mwc-icon>
+  <abbr
+    slot="action"
+    title="[edit]"
+  >
+    <mwc-icon-button icon="edit">
+    </mwc-icon-button>
+  </abbr>
   <div id="lnContainer">
     <ln-container>
     </ln-container>
@@ -42,6 +58,13 @@ snapshots["access-point-container with LN Elements and some LN Classes hidden lo
 `<action-pane tabindex="0">
   <mwc-icon slot="icon">
   </mwc-icon>
+  <abbr
+    slot="action"
+    title="[edit]"
+  >
+    <mwc-icon-button icon="edit">
+    </mwc-icon-button>
+  </abbr>
   <div id="lnContainer">
     <ln-container>
     </ln-container>
@@ -60,6 +83,13 @@ snapshots["access-point-container with LN Elements and all LN Classes hidden loo
 `<action-pane tabindex="0">
   <mwc-icon slot="icon">
   </mwc-icon>
+  <abbr
+    slot="action"
+    title="[edit]"
+  >
+    <mwc-icon-button icon="edit">
+    </mwc-icon-button>
+  </abbr>
   <div id="lnContainer">
   </div>
 </action-pane>
