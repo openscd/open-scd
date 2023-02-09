47827ccbe54d143a019c9816e7a8150b55fd0143
Pascal Wilbrink <pascal.wilbrink@alliander.com>
2023-02-09 14:18:40 +0100

    Added Sitipe Substation snapshot test

---
+++ test/unit/editors/sitipe/__snapshots__/sitipe-substation.test.snap.js
@@ -0,0 +1,172 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["sitipe-substation looks like the latest snapshot"] = 
+`<oscd-action-pane label="Vienna - Test project NBG">
+  <action-pane
+    label="0kV 
+      "
+    tabindex="0"
+  >
+    <div class="bayContainer">
+      <action-pane
+        label="CP8050 "
+        tabindex="0"
+      >
+        <div>
+          <action-icon
+            icon="developer_board"
+            tabindex="0"
+          >
+          </action-icon>
+        </div>
+      </action-pane>
+    </div>
+  </action-pane>
+  <action-pane
+    label="132kV 
+      "
+    tabindex="0"
+  >
+    <div class="bayContainer">
+      <action-pane
+        label="Vienna north "
+        tabindex="0"
+      >
+        <div>
+          <action-icon
+            icon="developer_board"
+            tabindex="0"
+          >
+          </action-icon>
+          <action-icon
+            icon="developer_board"
+            tabindex="0"
+          >
+          </action-icon>
+        </div>
+      </action-pane>
+      <action-pane
+        label="Vienna east "
+        tabindex="0"
+      >
+        <div>
+          <action-icon
+            icon="developer_board"
+            tabindex="0"
+          >
+          </action-icon>
+          <action-icon
+            icon="developer_board"
+            tabindex="0"
+          >
+          </action-icon>
+        </div>
+      </action-pane>
+      <action-pane
+        label="Vienna south "
+        tabindex="0"
+      >
+        <div>
+          <action-icon
+            icon="developer_board"
+            tabindex="0"
+          >
+          </action-icon>
+        </div>
+      </action-pane>
+      <action-pane
+        label="Vienna west "
+        tabindex="0"
+      >
+        <div>
+          <action-icon
+            icon="developer_board"
+            tabindex="0"
+          >
+          </action-icon>
+        </div>
+      </action-pane>
+      <action-pane
+        label="Graz north "
+        tabindex="0"
+      >
+        <div>
+        </div>
+      </action-pane>
+      <action-pane
+        label="Graz east "
+        tabindex="0"
+      >
+        <div>
+          <action-icon
+            icon="developer_board"
+            tabindex="0"
+          >
+          </action-icon>
+        </div>
+      </action-pane>
+      <action-pane
+        label="Graz south "
+        tabindex="0"
+      >
+        <div>
+          <action-icon
+            icon="developer_board"
+            tabindex="0"
+          >
+          </action-icon>
+          <action-icon
+            icon="developer_board"
+            tabindex="0"
+          >
+          </action-icon>
+        </div>
+      </action-pane>
+      <action-pane
+        label="Graz west "
+        tabindex="0"
+      >
+        <div>
+          <action-icon
+            icon="developer_board"
+            tabindex="0"
+          >
+          </action-icon>
+          <action-icon
+            icon="developer_board"
+            tabindex="0"
+          >
+          </action-icon>
+        </div>
+      </action-pane>
+      <action-pane
+        label="Salzburg north "
+        tabindex="0"
+      >
+        <div>
+          <action-icon
+            icon="developer_board"
+            tabindex="0"
+          >
+          </action-icon>
+        </div>
+      </action-pane>
+      <action-pane
+        label="Salzburg south "
+        tabindex="0"
+      >
+        <div>
+          <action-icon
+            icon="developer_board"
+            tabindex="0"
+          >
+          </action-icon>
+        </div>
+      </action-pane>
+    </div>
+  </action-pane>
+</oscd-action-pane>
+`;
+/* end snapshot sitipe-substation looks like the latest snapshot */
+
