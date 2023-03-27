abecd5e595830bf0dd138c52398995a75a387291
MarcvanRaalte <marc.van.raalte@alliander.com>
2023-03-27 16:39:34 +0200

    feat(process.test):test_added

--- test/unit/wizards/__snapshots__/process.test.snap.js
+++ test/unit/wizards/__snapshots__/process.test.snap.js
@@ -48,3 +48,49 @@ snapshots["Wizards for SCL Process element define an edit wizard that looks like
 `;
 /* end snapshot Wizards for SCL Process element define an edit wizard that looks like the the latest snapshot */
 
+snapshots["Wizards for SCL Process element define a create wizard that looks like the the latest snapshot"] = 
+`<mwc-dialog
+  defaultaction="next"
+  heading="[wizard.title.add]"
+  open=""
+  style="--mdc-dialog-min-width:calc(100% + 0px)"
+>
+  <div id="wizard-content">
+    <wizard-textfield
+      dialoginitialfocus=""
+      helper="[scl.name]"
+      label="name"
+      required=""
+      validationmessage="[textfield.required]"
+    >
+    </wizard-textfield>
+    <wizard-textfield
+      helper="[scl.desc]"
+      label="desc"
+      nullable=""
+    >
+    </wizard-textfield>
+    <wizard-textfield
+      helper="[scl.type]"
+      label="type"
+    >
+    </wizard-textfield>
+  </div>
+  <mwc-button
+    dialogaction="close"
+    label="[close]"
+    slot="secondaryAction"
+    style="--mdc-theme-primary: var(--mdc-theme-error)"
+  >
+  </mwc-button>
+  <mwc-button
+    icon="save"
+    label="[save]"
+    slot="primaryAction"
+    trailingicon=""
+  >
+  </mwc-button>
+</mwc-dialog>
+`;
+/* end snapshot Wizards for SCL Process element define a create wizard that looks like the the latest snapshot */
+
