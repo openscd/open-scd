084959842531eb8a75c908a59594784329b279fa
cad <christian.dinkel@omicronenergy.com>
2023-03-28 15:27:48 +0000

    fix(foundation): allow the dollar sign in names
    This allows users to save changes to elements with the
    dollar sign `$` in their `name` or `id` attributes, as is
    often used by vendors in data type template `id`s.

--- src/foundation.ts
+++ src/foundation.ts
@@ -2607,7 +2607,7 @@ const nameStartChar =
   '|[\u037F-\u1FFF]|[\u200C-\u200D]|[\u2070-\u218F]|[\u2C00-\u2FEF]' +
   '|[\u3001-\uD7FF]|[\uF900-\uFDCF]|[\uFDF0-\uFFFD]';
 const nameChar =
-  nameStartChar + '|[.0-9-]|\u00B7|[\u0300-\u036F]|[\u203F-\u2040]';
+  nameStartChar + '|[.0-9$-]|\u00B7|[\u0300-\u036F]|[\u203F-\u2040]';
 const name = nameStartChar + '(' + nameChar + ')*';
 const nmToken = '(' + nameChar + ')+';
 
