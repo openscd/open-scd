69cbd7ad533701075859eb547cb718874680997d
Pascal Wilbrink <pascal.wilbrink@alliander.com>
2023-10-18 15:57:39 +0200

    Chore: Added ConfDataSet to the Services Wizard

--- packages/open-scd/test/integration/wizards/services-wizard.test.ts
+++ packages/open-scd/test/integration/wizards/services-wizard.test.ts
@@ -10,63 +10,69 @@ describe('Wizards for SCL element Services', () => {
   let element: MockWizardEditor;
   let wizard: Wizard;
 
-  beforeEach(async () => {
-    element = await fixture(html`<mock-wizard-editor></mock-wizard-editor>`);
-    doc = await fetch('/test/testfiles/Services.scd')
-      .then(response => response.text())
-      .then(str => new DOMParser().parseFromString(str, 'application/xml'));
+  ['WithServices', 'WithServices2'].forEach(ied => {
+    beforeEach(async () => {
+      element = await fixture(html`<mock-wizard-editor></mock-wizard-editor>`);
+      doc = await fetch('/test/testfiles/Services.scd')
+        .then(response => response.text())
+        .then(str => new DOMParser().parseFromString(str, 'application/xml'));
 
-    wizard = editServicesWizard(
-      doc.querySelector('IED[name="WithServices"] Services')!
-    );
-
-    element.workflow.push(() => wizard);
-    await element.requestUpdate();
-  });
-
-  describe('define a Services wizards ', () => {
-    it('Services wizard to have 6 pages', () => {
-      expect(element.wizardUI.wizard.length).to.equal(6);
-    });
-    it('Services wizard to have 130 inputs', () => {
-      expect(element.wizardUI.wizard.flatMap(p => p.content).length).to.equal(
-        130
+      wizard = editServicesWizard(
+        doc.querySelector('IED[name="WithServices"] Services')!
       );
+
+      element.workflow.push(() => wizard);
+      await element.requestUpdate();
     });
 
-    [13, 22, 22, 23, 28, 22].forEach((inputs, idx) => {
-      it(`Services wizard ${idx + 1} to have ${inputs} inputs`, () => {
-        expect(element.wizardUI.wizard[idx].content!.length).to.equal(inputs);
+    describe(`IED [${ied}]: define a Services wizards`, () => {
+      it('Services wizard to have 6 pages', () => {
+        expect(element.wizardUI.wizard.length).to.equal(6);
       });
-    });
-
-    [0, 1, 2, 3, 4, 5].forEach(idx => {
-      it(`Wizard ${idx + 1} should look like snapshot`, () => {
-        expect(element.wizardUI.dialogs[idx]).to.equalSnapshot();
-      });
-    });
-  });
-
-  ['AP2', 'AP3', 'AP4', 'AP5', 'AP6'].forEach(accessPointName => {
-    describe('AccessPoint wizards for Scl element Services', () => {
-      beforeEach(async () => {
-        element = await fixture(
-          html`<mock-wizard-editor></mock-wizard-editor>`
+      it('Services wizard to have 130 inputs', () => {
+        expect(element.wizardUI.wizard.flatMap(p => p.content).length).to.equal(
+          134
         );
-        doc = await fetch('/test/testfiles/Services.scd')
-          .then(response => response.text())
-          .then(str => new DOMParser().parseFromString(str, 'application/xml'));
-
-        wizard = editServicesWizard(
-          doc.querySelector(`AccessPoint[name="${accessPointName}"] Services`)!
-        );
-
-        element.workflow.push(() => wizard);
-        await element.requestUpdate();
       });
 
-      it('should look like snapshot', () => {
-        expect(element.wizardUI.dialog).to.equalSnapshot();
+      [17, 22, 22, 23, 28, 22].forEach((inputs, idx) => {
+        it(`Services wizard ${idx + 1} to have ${inputs} inputs`, () => {
+          expect(element.wizardUI.wizard[idx].content!.length).to.equal(inputs);
+        });
+      });
+
+      [0, 1, 2, 3, 4, 5].forEach(idx => {
+        it(`Wizard ${idx + 1} should look like snapshot`, () => {
+          expect(element.wizardUI.dialogs[idx]).to.equalSnapshot();
+        });
+      });
+    });
+
+    ['AP2', 'AP3', 'AP4', 'AP5', 'AP6'].forEach(accessPointName => {
+      describe(`IED [${ied}]: AccessPoint wizards for Scl element Services`, () => {
+        beforeEach(async () => {
+          element = await fixture(
+            html`<mock-wizard-editor></mock-wizard-editor>`
+          );
+          doc = await fetch('/test/testfiles/Services.scd')
+            .then(response => response.text())
+            .then(str =>
+              new DOMParser().parseFromString(str, 'application/xml')
+            );
+
+          wizard = editServicesWizard(
+            doc.querySelector(
+              `AccessPoint[name="${accessPointName}"] Services`
+            )!
+          );
+
+          element.workflow.push(() => wizard);
+          await element.requestUpdate();
+        });
+
+        it('should look like snapshot', () => {
+          expect(element.wizardUI.dialog).to.equalSnapshot();
+        });
       });
     });
   });
