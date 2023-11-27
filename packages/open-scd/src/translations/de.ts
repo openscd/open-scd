dff67ba53eb740f912f51dbae21d83a41e1c3332
Juan Munoz <juan.munoz@alliander.com>
2023-11-27 13:22:45 +0100

    chore(104): validate IOA number in address-wizard (#1370)
    * chore(104): validate IOA number in address-wizard
    
    Signed-off-by: Juan Munoz <juancho0202@gmail.com>
    
    * test: added test for validation
    
    Signed-off-by: Juan Munoz <juancho0202@gmail.com>
    
    * chore(104): update validation message
    
    Signed-off-by: Juan Munoz <juancho0202@gmail.com>
    
    * test: updating validation message test
    
    Signed-off-by: Juan Munoz <juancho0202@gmail.com>
    
    * chore: adding typed `this`param to validateIOA fn
    
    Signed-off-by: Juan Munoz <juancho0202@gmail.com>
    
    * chore: removing unused attributes
    
    Signed-off-by: Juan Munoz <juancho0202@gmail.com>
    
    ---------
    
    Signed-off-by: Juan Munoz <juancho0202@gmail.com>

--- packages/open-scd/src/translations/de.ts
+++ packages/open-scd/src/translations/de.ts
@@ -521,6 +521,7 @@ export const de: Translations = {
         addAddress: '104-Adresse hinzufügen',
       },
       error: {
+        ioaConflict: 'IOA-Konflikt innerhalb der CASDU-Nummer gefunden',
         addAddressError:
           'Invalide Template Struktur, DAI kann nicht hinzugefügt werden (DO: "{{ doType }}", CDC: "{{ cdc }}", Structure: "{{ structure }}")',
       },
