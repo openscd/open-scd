78f038cb639c63cfea6102b6571f310cfbee7f45
Stef3st <steffen.van.den.driest@alliander.com>
2023-12-19 20:45:22 +0100

    chore: fix integration snapshots
    Signed-off-by: Stef3st <steffen.van.den.driest@alliander.com>

--- packages/open-scd/test/unit/wizards/__snapshots__/dai.test.snap.js
+++ packages/open-scd/test/unit/wizards/__snapshots__/dai.test.snap.js
@@ -84,49 +84,6 @@ snapshots["Wizards for SCL element DAI create DAI without Val Element looks like
 `;
 /* end snapshot Wizards for SCL element DAI create DAI without Val Element looks like the latest snapshot */
 
-snapshots["Wizards for SCL element DAI edit existing DAI with Val Element looks like the latest snapshot"] = 
-`<mwc-dialog
-  defaultaction="next"
-  heading="[dai.wizard.title.edit]"
-  open=""
-  style="--mdc-dialog-min-width:calc(100% + 0px)"
->
-  <div id="wizard-content">
-    <wizard-textfield
-      helper="[dai.wizard.valueHelper]"
-      id="Val"
-      label="Val"
-      max="255"
-      min="0"
-      type="number"
-    >
-    </wizard-textfield>
-    <wizard-textfield
-      disabled=""
-      id="daVal"
-      label="DA Template Value"
-      readonly=""
-    >
-    </wizard-textfield>
-  </div>
-  <mwc-button
-    dialogaction="close"
-    label="[close]"
-    slot="secondaryAction"
-    style="--mdc-theme-primary: var(--mdc-theme-error)"
-  >
-  </mwc-button>
-  <mwc-button
-    icon="edit"
-    label="[save]"
-    slot="primaryAction"
-    trailingicon=""
-  >
-  </mwc-button>
-</mwc-dialog>
-`;
-/* end snapshot Wizards for SCL element DAI edit existing DAI with Val Element looks like the latest snapshot */
-
 snapshots["Wizards for SCL element DAI edit existing DAI with Val Element having on value looks like the latest snapshot"] = 
 `<mwc-dialog
   defaultaction="next"
@@ -169,47 +126,4 @@ snapshots["Wizards for SCL element DAI edit existing DAI with Val Element having
 </mwc-dialog>
 `;
 /* end snapshot Wizards for SCL element DAI edit existing DAI with Val Element having on value looks like the latest snapshot */
-
-snapshots["Wizards for SCL element DAI edit existing DAI with Val Element having multiple setting group values looks like the latest snapshot"] = 
-`<mwc-dialog
-  defaultaction="next"
-  heading="[dai.wizard.title.edit]"
-  open=""
-  style="--mdc-dialog-min-width:calc(100% + 0px)"
->
-  <div id="wizard-content">
-    <wizard-textfield
-      helper="[dai.wizard.valueHelper]"
-      id="Val"
-      label="Val"
-      max="255"
-      min="0"
-      type="number"
-    >
-    </wizard-textfield>
-    <wizard-textfield
-      disabled=""
-      id="daVal"
-      label="DA Template Value"
-      readonly=""
-    >
-    </wizard-textfield>
-  </div>
-  <mwc-button
-    dialogaction="close"
-    label="[close]"
-    slot="secondaryAction"
-    style="--mdc-theme-primary: var(--mdc-theme-error)"
-  >
-  </mwc-button>
-  <mwc-button
-    icon="edit"
-    label="[save]"
-    slot="primaryAction"
-    trailingicon=""
-  >
-  </mwc-button>
-</mwc-dialog>
-`;
-/* end snapshot Wizards for SCL element DAI edit existing DAI with Val Element having multiple setting group values looks like the latest snapshot */
 
