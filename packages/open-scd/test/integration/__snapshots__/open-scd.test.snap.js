f994a5348e171eaf62747882799bbd42f00679b1
Pascal Wilbrink <pascal.wilbrink@alliander.com>
2023-12-20 14:13:49 +0100

    Reverted open-scd snapshot

--- packages/open-scd/test/integration/__snapshots__/open-scd.test.snap.js
+++ packages/open-scd/test/integration/__snapshots__/open-scd.test.snap.js
@@ -1,8 +1,7 @@
 /* @web/test-runner snapshot v1 */
 export const snapshots = {};
 
-snapshots["open-scd looks like its snapshot"] = 
-`<mwc-drawer
+snapshots['open-scd looks like its snapshot'] = `<mwc-drawer
   class="mdc-theme--surface"
   hasheader=""
   id="menu"
@@ -52,23 +51,6 @@ snapshots["open-scd looks like its snapshot"] =
     </mwc-list-item>
     <oscd-plugin679d81826289fb9a class="menu plugin">
     </oscd-plugin679d81826289fb9a>
-    <mwc-list-item
-      aria-disabled="false"
-      class="top"
-      graphic="icon"
-      iconid="folder_open"
-      mwc-list-item=""
-      tabindex="-1"
-    >
-      <mwc-icon slot="graphic">
-        folder_open
-      </mwc-icon>
-      <span>
-        OSCD Open
-      </span>
-    </mwc-list-item>
-    <oscd-plugina5597fe7cfa46aab class="menu plugin">
-    </oscd-plugina5597fe7cfa46aab>
     <mwc-list-item
       aria-disabled="true"
       class="top"
@@ -364,14 +346,6 @@ snapshots["open-scd looks like its snapshot"] =
   >
     <div class="landing_label">
       New project
-    </div>
-  </mwc-icon-button>
-  <mwc-icon-button
-    class="landing_icon"
-    icon="folder_open"
-  >
-    <div class="landing_label">
-      OSCD Open
     </div>
   </mwc-icon-button>
 </div>
@@ -856,22 +830,6 @@ snapshots["open-scd looks like its snapshot"] =
         create_new_folder
       </mwc-icon>
       New project
-    </mwc-check-list-item>
-    <mwc-check-list-item
-      aria-disabled="false"
-      class="official"
-      graphic="control"
-      hasmeta=""
-      left=""
-      mwc-list-item=""
-      selected=""
-      tabindex="-1"
-      value="https://openscd.github.io/oscd-open/oscd-open.js"
-    >
-      <mwc-icon slot="meta">
-        folder_open
-      </mwc-icon>
-      OSCD Open
     </mwc-check-list-item>
     <mwc-check-list-item
       aria-disabled="false"
@@ -1452,4 +1410,3 @@ snapshots["open-scd looks like its snapshot"] =
 </mwc-linear-progress>
 `;
 /* end snapshot open-scd looks like its snapshot */
-
