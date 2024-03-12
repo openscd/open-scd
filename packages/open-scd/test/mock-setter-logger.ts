81edd5876f778cbace88c3a0c83407d279da3b0a
Juan Munoz <juancho0202@gmail.com>
2024-03-13 00:13:46 +0100

    chore: removing unused files
    Signed-off-by: Juan Munoz <juancho0202@gmail.com>

--- packages/open-scd/test/mock-setter-logger.ts
+++
@@ -1,6 +0,0 @@
-import { LitElement, customElement } from 'lit-element';
-import { Editing } from '../src/Editing.js';
-import { Historing } from '../src/Historing.js';
-
-@customElement('mock-setter-logger')
-export class MockSetterLogger extends Editing(Historing(LitElement)) {}
