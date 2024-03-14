57c5af5ce2b8ca4424d2f743a2be490a947e9198
Tamás Russ <github@tamasruss.com>
2024-03-14 12:19:10 +0100

    ref: make sure everything is included in linting

--- packages/core/.eslintrc.js
+++ packages/core/.eslintrc.js
@@ -1,14 +1,17 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "standard-with-typescript",
-    "overrides": [],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-    }
+	"env": {
+		"browser": true,
+		"es2021": true
+	},
+	"extends": "standard-with-typescript",
+	"overrides": [],
+	"parserOptions": {
+		"ecmaVersion": "latest",
+		"sourceType": "module"
+	},
+	"rules": {
+		"indent": ["error", "tab"],
+		"@typescript-eslint/indent": ["error", "tab"],
+		"no-tabs": "off",
+	}
 }
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,10 +2,10 @@ export interface EditEvent {
 	element: Element
 }
 
-export function createEditEvent(element: Element): EditEvent {
+export function createEditEvent (element: Element): EditEvent {
 	return { element }
 }
 
-export function sum(a: number, b: number) {
+export function sum (a: number, b: number): number {
 	return a + b
 }
