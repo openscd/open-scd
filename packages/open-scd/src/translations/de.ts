2e89c8a83dcaa1b91fa226567f71f4cf1c9e801d
Juan Munoz <juancho0202@gmail.com>
2024-04-04 14:15:01 +0200

    chore: update translations for plug-ins selector in drawer menu help wanted with german translations for "plug-in" and "plug-ins"
    Signed-off-by: Juan Munoz <juancho0202@gmail.com>

--- packages/open-scd/src/translations/de.ts
+++ packages/open-scd/src/translations/de.ts
@@ -181,7 +181,7 @@ export const de: Translations = {
       'DataTypeTemplates können nicht validiert werden. Das Projekt muss die Version 2007B3 oder höher haben.',
   },
   plugins: {
-    heading: 'Erweiterungen',
+    heading: '???',
     editor: 'Editor',
     menu: 'Menüeintrag',
     requireDoc: 'Benötigt Dokument',
@@ -190,13 +190,13 @@ export const de: Translations = {
     bottom: 'unten',
     validator: 'Validator',
     add: {
-      heading: 'Benutzerdefinierte Erweiterung',
-      warning: `Hier können Sie benutzerdefinierte Erweiterungen hinzufügen.
+      heading: 'Benutzerdefinierte ???',
+      warning: `Hier können Sie benutzerdefinierte ??? hinzufügen.
                 OpenSCD übernimmt hierfür keine Gewähr.`,
       name: 'Name',
-      nameHelper: 'Lokaler Name der Erweiterung (frei wählbar)',
+      nameHelper: 'Lokaler Name der ??? (frei wählbar)',
       src: 'URL',
-      srcHelper: 'Die Erweiterungs-URL des Herstellers',
+      srcHelper: 'Die ???-URL des Herstellers',
     },
   },
   substation: {
