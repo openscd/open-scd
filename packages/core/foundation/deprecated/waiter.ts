449e58c6939f261d428895e8a5c1cca80b4a66c4
Juan Munoz <juancho0202@gmail.com>
2024-04-19 16:48:13 +0200

    feat(core): extract api from open-scd (events, types and interfaces) - places api into folder "foundation/deprecated" - (does not include code related to wizards or scl library)
    Signed-off-by: Juan Munoz <juancho0202@gmail.com>

---
+++ packages/core/foundation/deprecated/waiter.ts
@@ -0,0 +1,26 @@
+/** Represents some work pending completion, upon which `promise` resolves. */
+export interface PendingStateDetail {
+  promise: Promise<void>;
+}
+export type PendingStateEvent = CustomEvent<PendingStateDetail>;
+
+/**
+ * @deprecated
+ */
+export function newPendingStateEvent(
+  promise: Promise<void>,
+  eventInitDict?: CustomEventInit<Partial<PendingStateDetail>>
+): PendingStateEvent {
+  return new CustomEvent<PendingStateDetail>('pending-state', {
+    bubbles: true,
+    composed: true,
+    ...eventInitDict,
+    detail: { promise, ...eventInitDict?.detail },
+  });
+}
+
+declare global {
+  interface ElementEventMap {
+    ['pending-state']: PendingStateEvent;
+  }
+}
