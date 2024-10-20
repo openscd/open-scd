ff9e3ca3d27bd34df6a1d8d72d7080d7197925a9
Tamás Russ <github@tamasruss.com>
2024-10-20 15:47:49 +0200

    fix: code duplicate

--- packages/openscd/web-test-runner.config.mjs
+++ packages/openscd/web-test-runner.config.mjs
@@ -1,6 +1,5 @@
 import { playwrightLauncher } from '@web/test-runner-playwright';
 import { esbuildPlugin } from '@web/dev-server-esbuild';
-import { playwrightLauncher } from '@web/test-runner-playwright';
 
 export default /** @type {import("@web/test-runner").TestRunnerConfig} */ ({
   /** we run test directly on TypeScript files */
