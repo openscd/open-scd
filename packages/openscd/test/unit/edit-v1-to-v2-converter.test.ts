18a2061ac5cfbc4d3de0d5e98398a94bc613611f
Christopher Lepski <c.lepski@sprinteins.com>
2024-10-23 13:31:38 +0200

    test: Add tests

---
+++ packages/openscd/test/unit/edit-v1-to-v2-converter.test.ts
@@ -0,0 +1,145 @@
+import { html, fixture, expect } from '@open-wc/testing';
+
+import {
+  Create,
+  Delete,
+  EditorAction,
+  isCreate,
+  isDelete,
+  isMove,
+  isReplace,
+  isSimple,
+  isUpdate,
+  Move,
+  Replace,
+  SimpleAction,
+  Update,
+  createUpdateAction
+} from '@openscd/core/foundation/deprecated/editor.js';
+import { Edit, Insert, Remove, Update as UpdateV2 } from '@openscd/core';
+
+import { convertEditV1toV2 } from '../../src/addons/editor/edit-v1-to-v2-converter.js';
+
+
+describe('edit-v1-to-v2-converter', () => {
+  const doc = new DOMParser().parseFromString(
+    `
+    <Substation name="sub">
+      <Bay name="bay" desc="desc"></Bay>
+    </Substation>
+    <Substation name="sub2">
+    </Substation>
+    `,
+    'application/xml'
+  );
+  const substation = doc.querySelector('Substation')!;
+  const substation2 = doc.querySelector('Substation[name="sub2"]')!;
+  const bay = doc.querySelector('Bay')!;
+
+  it('should convert delete to remove', () => {
+    const deleteAction: Delete = {
+      old: {
+        parent: substation,
+        element: bay
+      }
+    };
+
+    const remove = convertEditV1toV2(deleteAction);
+
+    const expectedRemove: Remove = {
+      node: bay
+    };
+
+    expect(remove).to.deep.equal(expectedRemove);
+  });
+
+  it('should convert create to insert', () => {
+    const newBay = doc.createElement('Bay');
+    newBay.setAttribute('name', 'bay2');
+
+    const createAction: Create = {
+      new: {
+        parent: substation,
+        element: newBay
+      }
+    };
+
+    const insert = convertEditV1toV2(createAction);
+
+    const expectedInsert: Insert = {
+      parent: substation,
+      node: newBay,
+      reference: null
+    };
+
+    expect(insert).to.deep.equal(expectedInsert);
+  });
+
+  it('should convert update to updateV2', () => {
+    const newAttributes = {
+      name: 'newBayName',
+    };
+    const updateAction = createUpdateAction(bay, newAttributes);
+
+    const updateV2 = convertEditV1toV2(updateAction);
+
+    const expectedUpdateV2: UpdateV2 = {
+      element: bay,
+      attributes: newAttributes
+    };
+
+    expect(updateV2).to.deep.equal(expectedUpdateV2);
+  });
+
+  it('should convert move to insert', () => {
+    const moveAction: Move = {
+      old: {
+        parent: substation,
+        element: bay,
+        reference: null
+      },
+      new: {
+        parent: substation2,
+        reference: null
+      }
+    };
+
+    const insert = convertEditV1toV2(moveAction);
+
+    const expectedInsert: Insert = {
+      parent: substation2,
+      node: bay,
+      reference: null
+    };
+
+    expect(insert).to.deep.equal(expectedInsert);
+  });
+
+  it('should convert replace to complex action with remove and insert', () => {
+    const ied = doc.createElement('IED');
+    ied.setAttribute('name', 'ied');
+
+    const replace: Replace = {
+      old: {
+        element: bay
+      },
+      new: {
+        element: ied
+      }
+    };
+
+    const [ remove, insert ] = convertEditV1toV2(replace) as Edit[];
+
+    const expectedRemove: Remove = {
+      node: bay
+    };
+    const expectedInsert: Insert = {
+      parent: substation,
+      node: ied,
+      reference: bay.nextSibling
+    };
+
+    expect(remove).to.deep.equal(expectedRemove);
+    expect(insert).to.deep.equal(expectedInsert);
+  });
+});
