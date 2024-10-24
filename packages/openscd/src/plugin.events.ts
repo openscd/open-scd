8f1efba2dbb739654f8b33894ac71b256d0bfdd8
Tamás Russ <github@tamasruss.com>
2024-10-24 22:51:44 +0200

    feat: add plugin management events

--- packages/openscd/src/plugin.events.ts
+++ packages/openscd/src/plugin.events.ts
@@ -1,9 +1,13 @@
+import { Plugin, PluginKind } from './plugin.js';
+
 /**
  * The configure plugin event allows the plugin to request that OpenSCD core add, remove, or reconfigure a plugin.
  */
 export type ConfigurePluginDetail = {
   name: string;
-  kind: 'menu' | 'editor';
+  // The API describes only 'menu' and 'editor' kinds b
+  // but we still use the 'validator' too, so I just use the type PluginKind
+  kind: PluginKind;
   config: Plugin | null;
 };
 
@@ -13,7 +17,7 @@ export type ConfigurePluginEvent = CustomEvent<ConfigurePluginDetail>;
  * The combination of name and kind uniquely identifies the plugin to be configured.
  * If config is null, the plugin is removed. Otherwise, the plugin is added or reconfigured.
  */
-export function newConfigurePluginEvent(name: string, kind: 'menu' | 'editor', config: Plugin | null): ConfigurePluginEvent {
+export function newConfigurePluginEvent(name: string, kind: PluginKind, config: Plugin | null): ConfigurePluginEvent {
   return new CustomEvent<ConfigurePluginDetail>('oscd-configure-plugin', {
     bubbles: true,
     composed: true,
