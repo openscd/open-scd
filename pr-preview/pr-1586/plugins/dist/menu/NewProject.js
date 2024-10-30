e9be0ff0e785994a5a90a3e0a0a554b1992f8ed0
trusz <trusz@users.noreply.github.com>
2024-10-30 14:41:41 +0000

    Deploy preview for PR 1586 🛫

--- pr-preview/pr-1586/plugins/dist/menu/NewProject.js
+++ pr-preview/pr-1586/plugins/dist/menu/NewProject.js
@@ -9,13 +9,13 @@ import { newLogEvent } from '../../../_snowpack/link/packages/core/dist/foundati
 import { newEmptySCD, } from '../../../openscd/src/schemas.js';
 export default class NewProjectPlugin extends LitElement {
     createNewProject(inputs, wizard) {
-        let docName = inputs[0].value ?? "";
-        const acceptedFileExtension = [".ssd", ".scd", ".fsd"];
+        let docName = inputs[0].value ?? '';
+        const acceptedFileExtension = ['.ssd', '.scd', '.fsd'];
         const isValidFileFormat = acceptedFileExtension.some((extension) => {
             return inputs[0].value?.endsWith(extension);
         });
         if (!isValidFileFormat) {
-            docName = docName + ".scd";
+            docName = docName + '.scd';
         }
         const version = (wizard.shadowRoot.querySelector('mwc-list').selected
             .value);
