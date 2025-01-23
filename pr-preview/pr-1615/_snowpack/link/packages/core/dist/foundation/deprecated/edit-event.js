67866a913107cb0a033fbdbbfc039b9eba95748c
clepski <clepski@users.noreply.github.com>
2025-01-23 11:42:28 +0000

    Deploy preview for PR 1615 🛫

--- pr-preview/pr-1615/_snowpack/link/packages/core/dist/foundation/deprecated/edit-event.js
+++ pr-preview/pr-1615/_snowpack/link/packages/core/dist/foundation/deprecated/edit-event.js
@@ -1,18 +1,36 @@
+/**
+ * @deprecated Use the new edit event V2 API instead.
+ */
 export function isComplex(edit) {
     return edit instanceof Array;
 }
+/**
+ * @deprecated Use the new edit event V2 API instead.
+ */
 export function isInsert(edit) {
     return edit.parent !== undefined;
 }
+/**
+ * @deprecated Use the new edit event V2 API instead.
+ */
 export function isNamespaced(value) {
     return value !== null && typeof value !== 'string';
 }
+/**
+ * @deprecated Use the new edit event V2 API instead.
+ */
 export function isUpdate(edit) {
     return edit.element !== undefined;
 }
+/**
+ * @deprecated Use the new edit event V2 API instead.
+ */
 export function isRemove(edit) {
     return (edit.parent === undefined && edit.node !== undefined);
 }
+/**
+ * @deprecated Use the new edit event V2 API instead.
+ */
 export function newEditEvent(edit, initiator = 'user') {
     return new CustomEvent('oscd-edit', {
         composed: true,
