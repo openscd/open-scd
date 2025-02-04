f9e4aa651672681526b158750f879dfa5715c99c
trusz <trusz@users.noreply.github.com>
2025-02-04 14:02:50 +0000

    Remove preview for PR 1611 🛬

--- pr-preview/pr-1611/_snowpack/link/packages/core/dist/foundation/deprecated/validation.js
+++
@@ -1,8 +0,0 @@
-export function newValidateEvent(eventInitDict) {
-    return new CustomEvent('validate', {
-        bubbles: true,
-        composed: true,
-        ...eventInitDict,
-    });
-}
-//# sourceMappingURL=validation.js.map
