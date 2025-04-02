647d89308264727fda3d19091082378990c78afa
Rasul Adasov <rasul@futureblur.com>
2025-04-02 15:49:58 +0200

    feat(vite): add dynamicImport plugin and allow cors

--- packages/distribution/vite.config.js
+++ packages/distribution/vite.config.js
@@ -1,6 +1,20 @@
-export default {
+import { defineConfig } from "vite";
+import dynamicImport from "vite-plugin-dynamic-import";
+
+export default defineConfig({
+  plugins: [dynamicImport()],
+  server: {
+    cors: {
+      origin: '*', // Allow all origins
+      methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific methods
+      allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+      credentials: true,
+    }
+  },
   build: {
     outDir: "build",
     target: "es2022",
+    module: "es2022",
+    moduleResolution: "node"
   }
-}
+})
