041f4c037e3a8c95da68b62c2a22eab7c6211246
Christopher Lepski <c.lepski@sprinteins.com>
2025-06-11 13:49:26 +0200

    test: Adjust tests

--- packages/plugins/test/unit/editors/communication/__snapshots__/gse-editor.test.snap.js
+++ packages/plugins/test/unit/editors/communication/__snapshots__/gse-editor.test.snap.js
@@ -20,6 +20,7 @@ snapshots["Editor web component for GSE element looks like its latest snapshot"]
   </mwc-fab>
   <mwc-fab
     class="gse-move-button"
+    disabled=""
     icon="forward"
     mini=""
     slot="action"
