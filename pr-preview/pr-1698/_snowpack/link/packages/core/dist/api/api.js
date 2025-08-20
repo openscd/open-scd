2bb60c73a8e0aaff8c007b28cb45afc814796e16
clepski <clepski@users.noreply.github.com>
2025-08-20 12:44:20 +0000

    Remove preview for PR 1698 🛬

--- pr-preview/pr-1698/_snowpack/link/packages/core/dist/api/api.js
+++
@@ -1,7 +0,0 @@
-import { PluginStateApi } from './plugin-state-api.js';
-export class OscdApi {
-    constructor(pluginTag) {
-        this.pluginState = new PluginStateApi(pluginTag);
-    }
-}
-//# sourceMappingURL=api.js.map
